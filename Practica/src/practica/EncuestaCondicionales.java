b6fd0d92da2c4ec9f4908f7a0f8cf4cbde1d068c
acamus79 <adriancamus1979@gmail.com>
2021-07-02 21:48:19 -0300

    Primeros Pasos en Java

---
+++ Practica/src/practica/EncuestaCondicionales.java
@@ -0,0 +1,22 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package practica;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Adrian E. Camus
+ */
+public class EncuestaCondicionales {
+Scanner leer = new Scanner(System.in).useDelimiter("\n");
+int opinion;
+ 
+    
+    
+    
+}
