8d0a21b4d2717a3a8388f41c996e4513119be1c7
acamus79 <adriancamus1979@gmail.com>
2021-07-05 17:39:59 -0300

    ej4

---
+++ Guia Java Intro/src/guia/java/intro/ej_4.java
@@ -0,0 +1,26 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    4.Escribir un programa que pida una frase y la muestre toda 
+    en mayúsculas y después toda en minúsculas. 
+    Nota: investigar la función toUpperCase() y toLowerCase() en Java.
+
+*/
+package guia.java.intro;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
