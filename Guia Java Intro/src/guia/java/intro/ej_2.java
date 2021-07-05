5e4912b5a8867ef519832bb5c49d88b47d339342
acamus79 <adriancamus1979@gmail.com>
2021-07-05 17:38:24 -0300

    //comentarios

--- Guia Java Intro/src/guia/java/intro/ej_2.java
+++ Guia Java Intro/src/guia/java/intro/ej_2.java
@@ -2,11 +2,10 @@
  * Sin licencia .
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
- */
-/*
-2.Escribir un programa que pida dos números enteros por teclado y calcule 
-la suma de los dos. 
-El programa deberá después mostrar el resultado de la suma
+
+    2.Escribir un programa que pida dos números enteros por teclado y calcule 
+    la suma de los dos. 
+    El programa deberá después mostrar el resultado de la suma
 */
 
 
@@ -24,7 +23,7 @@ public class ej_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
         System.out.println("Ingrese dos numeros enteros");
         int n1 = leer.nextInt();
         int n2 = leer.nextInt();
