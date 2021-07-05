fdcea7da3faad472c3d336ad57cf934c0c9eb56d
acamus79 <adriancamus1979@gmail.com>
2021-07-05 17:37:54 -0300

    /* 3.Escribir un programa que pida tu nombre, lo guardeenuna variable y lo muestre por pantalla. */

--- Guia Java Intro/src/guia/java/intro/ej_3.java
+++ Guia Java Intro/src/guia/java/intro/ej_3.java
@@ -2,8 +2,14 @@
  * Sin licencia .
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
- */
+    
+    3.Escribir un programa que pida tu nombre, lo guarde
+    en una variable y lo muestre por pantalla.
+
+*/
 package guia.java.intro;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +21,14 @@ public class ej_3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        
+        System.out.println("Como te llamas?");
+        String nombre = leer.next();
+        System.out.println("Mucho gusto "+nombre);
+        
+        
     }
     
 }
