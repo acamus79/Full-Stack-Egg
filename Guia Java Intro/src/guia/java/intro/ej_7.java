8e467cc088079f004ed9840a0cc3ada37140ed46
acamus79 <adriancamus1979@gmail.com>
2021-07-05 18:27:04 -0300

    ej7

---
+++ Guia Java Intro/src/guia/java/intro/ej_7.java
@@ -0,0 +1,31 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+    
+    7.Implementar un programa que dado dos números enteros determine 
+    cuál es el mayor y lo muestre por pantalla.
+
+ */
+package guia.java.intro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Ingrese dos numeros enteros");
+        int n1 = leer.nextInt();
+        int n2 = leer.nextInt();
+        
+    }
+    
+}
