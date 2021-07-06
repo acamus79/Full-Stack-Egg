256636b1e98b74538990651fb6337341ac733aed
acamus79 <adriancamus1979@gmail.com>
2021-07-05 21:37:39 -0300

    ej15

---
+++ Guia Java Intro/src/guia/java/intro/ej_15.java
@@ -0,0 +1,42 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    15.Realizar un programa que pida dos números enteros positivos por teclado
+    y muestre por pantalla el siguiente menú: 
+    
+    MENU 
+    1. Sumar 
+    2. Restar
+    3. Multiplicar
+    4. Dividir
+    5. Salir
+    Elija opción:
+    
+    El usuario deberá elegir una opción y el programa deberá mostrar el 
+    resultado por pantalla y luego volver al menú. El programa deberá 
+    ejecutarse hasta que se elija la opción 5. Tener en cuenta que, si el 
+    usuario selecciona la opción 5, en vez de salir del programa 
+    directamente, se debe mostrar el siguiente mensaje de confirmación: 
+    ¿Está seguro que desea salir del programa (S/N)? 
+    Si el usuario selecciona el carácter ‘S’ se sale del programa, 
+    caso contrario se vuelve a mostrar el menú
+
+ */
+package guia.java.intro;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
