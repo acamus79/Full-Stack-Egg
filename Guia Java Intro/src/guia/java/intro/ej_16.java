5b63dbb1de23735f3299bb07928be17ff0fb3958
acamus79 <adriancamus1979@gmail.com>
2021-07-06 17:50:19 -0300

    ej16

--- Guia Java Intro/src/guia/java/intro/ej_16.java
+++ Guia Java Intro/src/guia/java/intro/ej_16.java
@@ -6,12 +6,14 @@
     16.Escriba un programa que lea 20 números. Si el número leído es igual 
     a cero se debe salir del bucle y mostrar el mensaje 
     "Se capturó el numero cero".  
-    El programa deberá calcular y mostrarel resultado de la suma de los 
+    El programa deberá calcular y mostrar el resultado de la suma de los 
     números leídos, pero si el número es negativo no debe sumarse. 
     Nota: recordar el uso de la sentencia break.
 
  */
 package guia.java.intro;
+
+import java.util.Scanner;
 
 /**
  *
@@ -23,7 +25,22 @@ public class ej_16 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        int suma = 0;
+        int n1;
+        
+        do {
+            System.out.println("Ingrese un numero");
+                n1 = leer.nextInt();
+                if (n1>0){
+                    suma = suma + n1;
+                }
+                                
+        }while (n1==0);
+        System.out.println("Se capturó el numero cero");
+        System.out.println("La suma de los numeros capturados es "+suma);
     }
-    
+        
+        
 }
