e7a97cc6a430099ad266132eae879619df2f0349
acamus79 <adriancamus1979@gmail.com>
2021-07-06 20:57:56 -0300

    Primera Guia Java Ejercicio 16

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_16.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_16.java
@@ -6,6 +6,7 @@
     16.Escriba un programa que lea 20 números. Si el número leído es igual 
     a cero se debe salir del bucle y mostrar el mensaje 
     "Se capturó el numero cero".  
+    
     El programa deberá calcular y mostrar el resultado de la suma de los 
     números leídos, pero si el número es negativo no debe sumarse. 
     Nota: recordar el uso de la sentencia break.
