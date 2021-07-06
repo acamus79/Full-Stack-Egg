0de137a4923fd0ef61b7711138aacddb3fff2da7
acamus79 <adriancamus1979@gmail.com>
2021-07-05 23:52:28 -0300

    ej16

---
+++ Guia Java Intro/src/guia/java/intro/ej_16.java
@@ -0,0 +1,29 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    16.Escriba un programa que lea 20 números. Si el número leído es igual 
+    a cero se debe salir del bucle y mostrar el mensaje 
+    "Se capturó el numero cero".  
+    El programa deberá calcular y mostrarel resultado de la suma de los 
+    números leídos, pero si el número es negativo no debe sumarse. 
+    Nota: recordar el uso de la sentencia break.
+
+ */
+package guia.java.intro;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
