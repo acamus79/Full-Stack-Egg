23e0fcc11dacd773feed465983d9c6fc419d62e3
acamus79 <adriancamus1979@gmail.com>
2021-07-05 23:56:29 -0300

    ej17

---
+++ Guia Java Intro/src/guia/java/intro/ej_17.java
@@ -0,0 +1,34 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    17.Realizar un programa que simule el funcionamiento de un dispositivo RS232,
+    este tipo de dispositivo lee cadenas enviadas por el usuario. Las cadenas
+    deben llegar con un formato fijo: tienen que ser de un máximo de 5 caracteres
+    de largo, el primer carácter tiene que ser X y el último tiene que ser una O.
+    Las secuencias leídas que respeten el formato se consideran correctas, 
+    la secuencia especial “&&&&&” marca el final de los envíos (llamémosla FDE), 
+    y toda secuencia distinta de FDE, que no respete el formato se considera 
+    incorrecta. Al finalizar el proceso, se imprime un informe indicando la cantidad
+    de lecturas correctas e incorrectas recibidas. 
+    Para resolver el ejercicio deberá investigar cómo se utilizan las siguientes 
+    funciones de JavaSubstring(),Length(), equals().
+
+ */
+package guia.java.intro;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
