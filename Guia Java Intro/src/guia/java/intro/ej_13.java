0cb0f71225ae547c30a4742f01030791306e815f
acamus79 <adriancamus1979@gmail.com>
2021-07-05 21:04:38 -0300

    ej13

---
+++ Guia Java Intro/src/guia/java/intro/ej_13.java
@@ -0,0 +1,27 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    13.Escriba un programa que valide si una nota está entre 0 y 10, sino 
+    está entre 0 y 10 la nota se pedirá de nuevo hasta que la nota sea correcta. 
+
+ */
+package guia.java.intro;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
