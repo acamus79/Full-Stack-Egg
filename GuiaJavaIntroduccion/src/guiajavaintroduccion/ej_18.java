008fb73d805adb75667e6765b477756007602e3a
acamus79 <adriancamus1979@gmail.com>
2021-07-07 20:21:06 -0300

    Primera Guia Java Ejercicio 18

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_18.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_18.java
@@ -39,21 +39,22 @@ public class ej_18 {
                         
               for (int i=0;i<=9;i++){
                   a++;
-                  TimeUnit.SECONDS.sleep(1);
-                  if (i==3){
-                      
-                  }
+                  String convI = String.valueOf(i); //convierto i a cadena
+                    if (i==3){//si i toma el valor de 3 su equivalente convertido le asigno la letra E
+                    convI="E";
+                    }
                   for (int j=0;j<=9;j++){
-                      TimeUnit.SECONDS.sleep(1);
-                      if (j==3){
-                          
+                     String convJ = String.valueOf(j);//convierto j a cadena
+                      if (j==3){//si j toma el valor de 3 su equivalente convertido le asigno la letra E
+                        convJ = "E";
                       }   
                       for (int k=0;k<=9;k++){
-                          //TimeUnit.SECONDS.sleep(1);
+                          String convK = String.valueOf(k);//convierto k a cadena
+                          TimeUnit.SECONDS.sleep(1);//mando a dormir un segundo para que parezca un reloj
                           if (k==3){
-                              
+                              convK = "E";//si k toma el valor de 3 su equivalente convertido le asigno la letra E
                           }
-                      System.out.println("CONTADOR: "+ i+" - "+j+" - "+k); 
+                      System.out.println("CONTADOR: "+ convI+" - "+convJ+" - "+convK); 
                       }
                   }
                   
