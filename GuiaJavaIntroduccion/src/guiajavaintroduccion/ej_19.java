c20efa52bd14c584304f0b5f653d80b66379f8a6
acamus79 <adriancamus1979@gmail.com>
2021-07-07 20:36:03 -0300

    Primera Guia Java Ejercicio 19

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_19.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_19.java
@@ -2,8 +2,20 @@
  * Sin licencia .
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    19.Dibujar un cuadrado de N elementos por lado utilizando el carácter “*”. 
+    Por ejemplo, si el cuadrado tiene 4 elementos por lado se deberá 
+    dibujar lo siguiente:
+    
+    * * * *
+    *     *
+    *     *
+    * * * *
+
  */
 package guiajavaintroduccion;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,6 +28,24 @@ public class ej_19 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+                int a;
+		int b;
+		int n;
+		System.out.println("ingrese un entero");
+		n = leer.nextInt();
+		for (a=1;a<=n;a++) {
+			for (b=1;b<=n;b++) {
+				if (a==1 || a==n || b==1 || b==n) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println(" ");
+		}
+	}
+
+        
     }
     
-}
