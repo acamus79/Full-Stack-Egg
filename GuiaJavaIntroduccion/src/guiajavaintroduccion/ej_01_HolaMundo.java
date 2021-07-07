893ad21c6b288876969b1cf67ab5ed7fca40bf53
acamus79 <adriancamus1979@gmail.com>
2021-07-06 21:02:54 -0300

    Primera Guia Java Hola Mundo!!

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_01_HolaMundo.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_01_HolaMundo.java
@@ -20,14 +20,13 @@ public class ej_01_HolaMundo {
         //se crea una un objeto de la clase scanner que se utiliza para leer datos del usuario
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
         String nombre;//variable de tipo cadena 
+        
         //Mensaje por pantalla pidiendo ingresar el nombre
         System.out.println("Ingresa tu nombre");
         
         nombre = leer.next();
         
         System.out.println("Hola Mundo, soy"+nombre+"y estoy programando en Java");
-        
-        
         
     }
     
