be582ecde7323d7a066e527effa276c2059c997b
acamus79 <adriancamus1979@gmail.com>
2021-07-07 20:40:41 -0300

    Primera Guia Java Ejercicio 20

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_20.java
@@ -0,0 +1,31 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    20.Realizar un programa que lea 4 números (comprendidos entre 1 y 20) 
+    e imprima el número ingresado seguido de tantos asteriscos como indique 
+    su valor. 
+    Por ejemplo:
+    5 *****
+    3 ***
+    11 ***********
+    2 **
+
+ */
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
