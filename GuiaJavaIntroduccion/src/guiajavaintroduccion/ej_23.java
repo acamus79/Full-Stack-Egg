12777981d3f3da4ec2fa384981c222609d815123
acamus79 <adriancamus1979@gmail.com>
2021-07-08 18:45:34 -0300

    Primera Guia Java Ejercicio 23

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_23.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_23.java
@@ -11,6 +11,8 @@
  */
 package guiajavaintroduccion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Adrian E. Camus
@@ -20,8 +22,28 @@ public class ej_23 {
     /**
      * @param args the command line arguments
      */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        
+        int n;
+        System.out.println("Ingresa el tamañ del arreglo");
+        n=leer.nextInt();
+        
+        int[] vectorN = new int[n];
+        
+        //lleno con enteros aleatorios del 1 al 60
+        for (int i=0;i<n;i++){
+        //fuerzo que los generados sean enteros con (in) y luego limito que sean
+        //desde 0 a 60 los aleatorios pero al final le sumo +1 para que nunca
+        //me toque el 0 como aleatorio
+            vectorN[i]=(int)(Math.random()*60)+1;;
+        }
+        
+        System.out.println("Ingresa el numero a buscar");
+        n=leer.nextInt();
+    
+    
     }
     
 }
