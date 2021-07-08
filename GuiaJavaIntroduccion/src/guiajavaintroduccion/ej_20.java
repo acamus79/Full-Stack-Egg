a246f0b2c455f62749d4970030628c347cde0289
acamus79 <adriancamus1979@gmail.com>
2021-07-07 21:29:25 -0300

    Primera Guia Java Ejercicio 20

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_20.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_20.java
@@ -17,6 +17,8 @@
  */
 package guiajavaintroduccion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Adrian E. Camus
@@ -28,6 +30,26 @@ public class ej_20 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        int contador = 0;
+        int num;
+
+        while (contador < 4) {
+            System.out.print("Ingrese un número: ");
+            num = leer.nextInt();
+            for (int i = 0; i <= num; i++) {
+                if (i == 0) {
+                    System.out.print(num);
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+            contador = contador + 1;
+        }
+
     }
-    
-}
+        
+        
+    }
+
