084bf418a279273afc345b9c49abf9ec86647433
acamus79 <adriancamus1979@gmail.com>
2021-07-07 21:32:10 -0300

    Primera Guia Java Ejercicio 21

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_21.java
@@ -0,0 +1,33 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    21.Crea una aplicación que a través de una función nos convierta una 
+    cantidad de euros introducida por teclado a otra moneda, estas pueden 
+    ser a dólares, yenes o libras. La función tendrá como parámetros, la 
+    cantidad de euros y la moneda a converir que será una cadena, este no 
+    devolverá ningún valor y mostrará un mensaje indicando el cambio 
+    (void).
+    El cambio de divisas es: 
+    * 0.86 libras es un 1 €
+    * 1.28611 $ es un 1 €
+    * 129.852 yenes es un 1 €
+
+ */
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
