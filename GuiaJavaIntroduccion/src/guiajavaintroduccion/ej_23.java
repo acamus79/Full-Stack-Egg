f5a69bbe0f5a06eeeaffc2d8a887a0a50323c670
acamus79 <adriancamus1979@gmail.com>
2021-07-08 18:19:43 -0300

    Primera Guia Java Ejercicio 23

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_23.java
@@ -0,0 +1,27 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Realizar un algoritmo que rellene un vector de tamaño N con valores 
+    aleatorios y le pida al usuario un numero a buscar en el vector. 
+    El programa mostrará donde se encuentra el numero y si se encuentra 
+    repetido
+
+ */
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
