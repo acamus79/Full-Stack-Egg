da8b9637bb2a4be55f8bae7d7a3cf819a0b17e14
acamus79 <adriancamus1979@gmail.com>
2021-07-08 18:44:39 -0300

    ejemplo Matriz

--- EjemplosIniciales/src/ejemplosiniciales/Matriz.java
+++ EjemplosIniciales/src/ejemplosiniciales/Matriz.java
@@ -15,7 +15,29 @@ public class Matriz {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int matriz[][] = new int[3][3]; // Le ponemos la dimension a la matriz, 
+        // un 3 para las filas o otro para las columnas
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                matriz[i][j] = 6; // Le asignamos a cada posicion de la matriz
+                // en sus filas
+
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                // Usamos la i y j para pasar por todos los elementos y mostrarlos
+                // Mostrando la matriz por sus filas
+                System.out.print("[" + matriz[i][j] + "]");
+
+            }
+            System.out.println(" ");
+        }
     }
     
 }
