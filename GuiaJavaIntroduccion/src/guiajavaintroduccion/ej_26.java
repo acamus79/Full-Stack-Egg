738658ec2f3f10cc1ba4ff97726b996bfac57ab0
acamus79 <adriancamus1979@gmail.com>
2021-07-10 14:07:37 -0300

    Primera Guia Java Ejercicio 26

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_26.java
@@ -0,0 +1,35 @@
+/*
+ * Sin licencia .
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Realice un programa que compruebe si una matriz dada es anti simétrica. 
+    Se dice que una matriz A es anti simétrica cuando ésta es igual a su 
+    propia traspuesta, pero cambiada de signo. Es decir, A es anti simétrica 
+    si A = -AT. La matriz traspuesta de una matriz A se denota por AT y se 
+    obtiene cambiando sus filas por columnas (o viceversa). 
+    
+    Matriz          Matriz Traspuesta
+    0   -2  4       0   2   -4
+    2   0   2       -2  0   -2
+    -4  -2  0       4   2   0
+
+    Ejemplo: En este caso la matriz es anti simétrica. 
+
+ */
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
