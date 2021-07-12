048a33e1e196da64b79f223bc2961fc1d9b2aa0b
acamus79 <adriancamus1979@gmail.com>
2021-07-12 19:16:58 -0300

    Ejercicio 28

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_28.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_28.java
@@ -2,6 +2,20 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    28.Dadas dos matrices cuadradas de números enteros, la matriz M de 10x10 y
+    la matriz P de 3x3, se solicita escribir un programa en el cual se 
+    compruebe si la matriz P está contenida dentro de la matriz M. Para ello 
+    se debe verificar si entre todas las submatrices de 3x3 que se pueden 
+    formar en la matriz M, desplazándose por filas o columnas, existe al menos 
+    una que coincida con la matriz P. En ese caso, el programa debe indicar la 
+    fila y la columna de la matriz M en la cual empieza el primer elemento de 
+    la submatriz P.
+
+    Ejemplo:
+
+    
+
  */
 package guiajavaintroduccion;
 
