6eecffffb650e8434980918681b30c7a51b1c75b
acamus79 <adriancamus1979@gmail.com>
2021-07-13 20:06:35 -0300

    Guia 1 Java Extra 03

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_03.java
@@ -0,0 +1,31 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Elaborar un algoritmo en el cuál se ingrese una letra y se detecte si se 
+    trata de una vocal. Caso contrario mostrar un mensaje. Nota: investigar 
+    la función equals() de la clase String.
+
+ */
+
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_extra_03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+        
+        
+        
+        
+    }
+
+}
