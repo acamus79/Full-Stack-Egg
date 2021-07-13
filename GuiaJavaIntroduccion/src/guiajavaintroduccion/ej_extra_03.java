263576e991a2679164a524e8cc62800354fa9215
acamus79 <adriancamus1979@gmail.com>
2021-07-13 20:19:42 -0300

    Guia 1 Java Extra 03

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_03.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_03.java
@@ -11,6 +11,9 @@
 
 package guiajavaintroduccion;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author Adrian E. Camus
@@ -20,12 +23,29 @@ public class ej_extra_03 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // El Código va AQUI!!!
-        
-        
-        
-        
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Ingresa una letra");
+        String var = leer.nextLine();
+        var = var.toUpperCase();
+        var = var.substring(0, 1);
+        switch (var){
+                case "A":System.out.println(var+" Es la primera Vocal");
+                break;
+                case "E":System.out.println(var+" Es la segunda Vocal");
+                break;
+                case "I":System.out.println(var+" Es la tercer Vocal");
+                break;
+                case "O":System.out.println(var+" Es la cuarta Vocal");
+                break;
+                case "U":System.out.println(var+" Es la quinta Vocal");
+                break;
+                default:
+                            System.out.println("No hay vocal");
+                            System.out.println("Presione ENTER para continuar");
+                            System.in.read();// espera un Enter, no cualquier tecla
+        }    
     }
 
 }
