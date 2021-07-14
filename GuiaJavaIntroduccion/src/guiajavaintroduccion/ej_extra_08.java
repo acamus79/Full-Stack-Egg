097c4f175e2ee93b943cd69bcd3fa929c2f6e71e
acamus79 <adriancamus1979@gmail.com>
2021-07-14 20:36:42 -0300

    Guia 1 Java Extra 08

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_08.java
@@ -0,0 +1,29 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Escriba un programa que lea números enteros. Si el número es múltiplo de 
+    cinco debe detener la lectura y mostrar la cantidad de números leídos, 
+    la cantidad de números pares y la cantidad de números impares. Al igual 
+    que en el ejercicio anterior los números negativos no deben sumarse. 
+    Nota: recordar el uso de la sentencia break.
+
+ */
+
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_extra_08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
