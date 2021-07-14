d04c21930439b6218af3777457623ece63965269
acamus79 <adriancamus1979@gmail.com>
2021-07-13 22:15:29 -0300

    Guia 1 Java Ejercicio 27

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_27.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_27.java
@@ -46,11 +46,11 @@ public class ej_27 {
                 System.out.println(" ");
                  }
      }
-    
-    
+        
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
