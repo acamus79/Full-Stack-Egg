9cc3ac732f3adbeb5dd375dd09d557b686b79624
acamus79 <adriancamus1979@gmail.com>
2021-07-13 21:40:55 -0300

    Guia 1 Java Extra 04

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_04.java
@@ -0,0 +1,32 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Elaborar un algoritmo en el cuál se ingrese un número entre 1 y 10 y se 
+    muestre su equivalente en romano.
+
+ */
+
+package guiajavaintroduccion;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_extra_04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+        
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Ingresa un numero letra");
+        
+    }
+
+}
