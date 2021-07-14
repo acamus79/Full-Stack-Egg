f15a337e53f53d9b5a839a19cd18773eb67e64e5
acamus79 <adriancamus1979@gmail.com>
2021-07-13 22:20:05 -0300

    Guia 1 Java Ejercicio 28

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_28.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_28.java
@@ -29,7 +29,10 @@ public class ej_28 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        
+        
+        
     }
     
 }
