a3ccae36d120d11dc70b6d49a43debe2f3d32281
acamus79 <adriancamus1979@gmail.com>
2021-07-13 22:20:20 -0300

    Guia 1 Java Extra 05

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_05.java
@@ -0,0 +1,37 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    5.Una obra social tiene tres clases de socios: 
+
+    * Los socios tipo ‘A’ abonan una cuota mayor, pero tienen un 50% de 
+    descuento en todos los tipos de tratamientos. 
+    * Los socios tipo ‘B’ abonan una cuota moderada y tienen un 35% de 
+    descuento para los mismos tratamientos que los socios del tipo A. 
+    * Los socios que menos aportan, los de tipo ‘C’, no reciben descuentos 
+    sobre dichos tratamientos. 
+
+    Solicite una letra (carácter) que representa la clase de un socio, y 
+    luego un valor real que represente el costo del tratamiento 
+    (previo al descuento) y determine el importe en efectivo a pagar 
+    por dicho socio.
+
+ */
+
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_extra_05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
