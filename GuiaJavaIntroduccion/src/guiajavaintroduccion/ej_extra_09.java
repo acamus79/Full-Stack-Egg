4fa33d50b31233df4393c3aa205007650a04f95a
acamus79 <adriancamus1979@gmail.com>
2021-07-14 20:58:36 -0300

    Guia 1 Java Extra 09

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_09.java
@@ -0,0 +1,39 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Simular la división usando solamente restas. Dados dos números enteros 
+    mayores que uno, realizar un algoritmo que calcule el cociente y el 
+    residuo usando sólo restas. Método: Restar el dividendo del divisor 
+    hasta obtener un resultado menor que el divisor, este resultado es el 
+    residuo, y el número de restas realizadas es el cociente. 
+    Por ejemplo: 50 / 13: 
+    50 –13 = 37 una resta realizada 
+    37 –13 = 24 dos restas realizadas 
+    24 –13 = 11 tres restas realizadas 
+    dado que 11 es menor que 13, 
+    entonces: el residuo es 11 y el cociente es 3
+
+ */
+
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_extra_09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+        
+        
+        
+        
+    }
+
+}
