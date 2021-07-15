9d81ea4467d1fd3e1a4d109efde5b5aae5a5fabb
acamus79 <adriancamus1979@gmail.com>
2021-07-15 17:25:02 -0300

    Guia 1 Java Extra 10

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_10.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_10.java
@@ -2,6 +2,14 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Realice un programa para que el usuario adivine el resultado de una 
+    multiplicación entre dos números generados aleatoriamente entre 0 y 10. 
+    El programa debe indicar al usuario si su respuesta es o no correcta. 
+    En caso que la respuesta sea incorrecta se debe permitir al usuario 
+    ingresar su respuesta nuevamente. Para realizar este ejercicio 
+    investigue como utilizar la función Math.random()de Java.
+
  */
 
 package guiajavaintroduccion;
