1e1254fd9172bce03a601f1b17c38ddb5f9780bc
acamus79 <adriancamus1979@gmail.com>
2021-07-15 17:21:15 -0300

    Guia 1 Java Extra 09

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_09.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_09.java
@@ -17,7 +17,6 @@
     entonces: el residuo es 11 y el cociente es 3
 
  */
-
 package guiajavaintroduccion;
 
 import java.util.Scanner;
@@ -33,17 +32,31 @@ public class ej_extra_09 {
      */
     public static void main(String[] args) {
         // El Código va AQUI!!!
+        int resto, cociente = 0;
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
         System.out.println("Ingresa el dividendo");
         int dividendo = leer.nextInt();
         System.out.println("Ingresa el divisor");
         int divisor = leer.nextInt();
-        
-        
-        
-        
-        
-        
+
+        resto = dividendo;
+
+        if (dividendo > divisor) {
+
+            while (resto > divisor) {
+                resto = resto - divisor;
+                cociente++;
+            }
+        } else {
+            System.out.println("El dividendo debe ser mayor que el divisor");
+        }
+        if (resto==divisor){
+            cociente++;
+            resto=0;
+        }
+        System.out.println(dividendo + " / " + divisor + " = " + cociente);
+        System.out.println("con un resto de " + resto);
+
     }
 
 }
