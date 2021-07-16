fae1d3ba86b837cd604de3eec55381865de3822d
acamus79 <adriancamus1979@gmail.com>
2021-07-16 20:14:35 -0300

    Guia 1 Java Extra 12

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_12.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_12.java
@@ -15,6 +15,8 @@
 
 package guiajavaintroduccion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Adrian E. Camus
@@ -26,6 +28,20 @@ public class ej_extra_12 {
      */
     public static void main(String[] args) {
         // El Código va AQUI!!!
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        int contador = 0;
+        System.out.println("Ingresa un numero entero");
+        int num = leer.nextInt();
+        
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1 + " ");
+            }
+            System.out.println("");
+        }
+        
+        
+        
     }
 
 }
