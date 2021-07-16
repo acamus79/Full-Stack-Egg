14a0a57ab7a2818f23fc22e22b1edc0c28f97d88
acamus79 <adriancamus1979@gmail.com>
2021-07-16 19:17:24 -0300

    Guia 1 Java Extra 12

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_12.java
@@ -0,0 +1,31 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Crear un programa que dibuje una escalera de números, donde cada línea 
+    de números comience en uno y termine en el número de la línea. 
+    Solicitar la altura de la escalera al usuario al comenzar. 
+    Ejemplo: si se ingresa el número 3: 
+    1
+    12
+    123
+
+ */
+
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_extra_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
