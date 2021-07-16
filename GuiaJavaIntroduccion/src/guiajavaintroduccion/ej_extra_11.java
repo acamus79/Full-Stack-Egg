dae4cc3d5472446ee716fc176bc2a2ca9cf1a066
acamus79 <adriancamus1979@gmail.com>
2021-07-16 19:03:31 -0300

    Guia 1 Java Extra 11

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_11.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_11.java
@@ -11,7 +11,6 @@
     números o resultados.
 
  */
-
 package guiajavaintroduccion;
 
 import java.util.Scanner;
@@ -28,17 +27,16 @@ public class ej_extra_11 {
     public static void main(String[] args) {
         // El Código va AQUI!!!
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
-        int contador=0;
+        int contador = 0;
         System.out.println("Ingresa un numero entero");
         int num = leer.nextInt();
-        
-        while (num>0){
-            num = num/10;
+
+        while (num > 0) {
+            num = num / 10;
             contador++;
         }
-		
         System.out.println("El numero ingresado tiene " + contador + " dígitos");
-        
+
     }
 
 }
