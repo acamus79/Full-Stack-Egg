f818f2fe1a49e3c60122915cde725e2ad7095ec9
acamus79 <adriancamus1979@gmail.com>
2021-07-16 20:22:39 -0300

    Guia 1 Java Extra 13

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_13.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_13.java
@@ -9,8 +9,9 @@
     media de edad de los hijos de todas las familias.
 
  */
-
 package guiajavaintroduccion;
+
+import java.util.Scanner;
 
 /**
  *
@@ -23,6 +24,27 @@ public class ej_extra_13 {
      */
     public static void main(String[] args) {
         // El Código va AQUI!!!
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        int familias, hijos, hijosTotal;
+        double edad;
+        double edadMedia = 0;
+
+        edad = 0;
+        hijosTotal = 0;
+        System.out.println("Ingrese la cantidad de familias");
+        familias = leer.nextInt();
+        for (int i = 0; i < familias; i++) {
+            System.out.println("Ingrese la cantidad de hijos que tiene");
+            hijos = leer.nextInt();
+            for (int j = 0; j < hijos; j++) {
+                System.out.println("Ingrese la edad de su hijo " + (int) (j + 1));
+                edad += leer.nextInt();
+                hijosTotal += 1;
+            }
+        }
+        edadMedia = Math.round(edad / hijosTotal);
+        System.out.println("La edad media de todos los hijos es de " + (int) edadMedia);
+        leer.close();//cierro el teclado
     }
 
 }
