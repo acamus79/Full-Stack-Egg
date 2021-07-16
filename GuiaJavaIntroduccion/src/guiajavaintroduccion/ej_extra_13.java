5c9d1d7363d8c78f7669997d6eafb6f678ebb705
acamus79 <adriancamus1979@gmail.com>
2021-07-16 20:16:53 -0300

    Guia 1 Java Extra 13

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_13.java
@@ -0,0 +1,28 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Se dispone de un conjunto de N familias, cada una de las cuales tiene 
+    una M cantidad de hijos. Escriba un programa que pida la cantidad de 
+    familias y para cada familia la cantidad de hijos para averiguar la 
+    media de edad de los hijos de todas las familias.
+
+ */
+
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_extra_13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
