3c7dbd804ceae8b42ad7045e1a7d9a3a5959e8ef
acamus79 <adriancamus1979@gmail.com>
2021-07-16 19:57:02 -0300

    Guia 1 Java Ejercicio 16

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_16.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_16.java
@@ -26,25 +26,28 @@ public class ej_16 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
         int suma = 0;
-        int n1;
-        
+        int n1, cont = 0;
+
         do {//hacer
-            System.out.println("Ingrese un numero");
-                n1 = leer.nextInt();
-                //si n1 es mayor que cero entonces hace una sumatoria tipo acumulacion
-                //si es menor que cero quiere decir que es negativo entonces no hace nada
-                if (n1>0){
-                    suma = suma + n1;
-                }
-                                
-        }while (n1!=0);//mientras n1 sea distinto de cero
+            cont++;
+            System.out.println("Ingrese el numero " + cont);
+            n1 = leer.nextInt();
+
+            //si n1 es mayor que cero entonces hace una sumatoria tipo acumulacion
+            //si es menor que cero quiere decir que es negativo entonces no hace nada
+            if (n1 > 0) {
+                suma = suma + n1;
+            } else if (n1 == 0) {
+                System.out.println("Se capturó el numero cero");
+                break;
+            }
+
+        } while (cont < 20);//mientras n1 sea distinto de cero
         leer.close();//cierro el scanner porque ya no lo voy a usar mas
-        System.out.println("Se capturó el numero cero");
-        System.out.println("La suma de los numeros capturados es "+suma);
+        System.out.println("La suma de los numeros capturados es " + suma);
     }
-        
-        
+
 }
