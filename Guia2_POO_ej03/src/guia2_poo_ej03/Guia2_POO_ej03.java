c124a3f97689be9f9c56ddcecd95ab887069ad47
acamus79 <adriancamus1979@gmail.com>
2021-07-19 19:09:59 -0300

    Guia 2 Java POO Ejercicio 03

---
+++ Guia2_POO_ej03/src/guia2_poo_ej03/Guia2_POO_ej03.java
@@ -0,0 +1,22 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package guia2_poo_ej03;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Guia2_POO_ej03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
