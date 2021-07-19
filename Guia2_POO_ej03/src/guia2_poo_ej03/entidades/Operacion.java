d4c783a457e96179fcd6c56fe635711bebce596e
acamus79 <adriancamus1979@gmail.com>
2021-07-19 19:28:15 -0300

    Guia 2 Java POO Ejercicio 03

--- Guia2_POO_ej03/src/guia2_poo_ej03/entidades/Operacion.java
+++ Guia2_POO_ej03/src/guia2_poo_ej03/entidades/Operacion.java
@@ -10,5 +10,33 @@ package guia2_poo_ej03.entidades;
  * @author Adrian E. Camus
  */
 public class Operacion {
+    private int numero1;
+    private int numero2;
+
+    public Operacion() {
+    }
+
+    public Operacion(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+    
+    
     
 }
