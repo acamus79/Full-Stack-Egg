d4ca4223ee7f5c40c8d1414f0b6be8052d0b1330
acamus79 <adriancamus1979@gmail.com>
2021-07-19 19:44:15 -0300

    Guia 2 Java POO Ejercicio 03

--- Guia2_POO_ej03/src/guia2_poo_ej03/entidades/Operacion.java
+++ Guia2_POO_ej03/src/guia2_poo_ej03/entidades/Operacion.java
@@ -2,6 +2,11 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+ a) Método constructor con todos los atributos pasados por parámetro.
+ b) Metodo constructor sin los atributos pasados por parámetro.
+ c) Métodos get y set.    
+
  */
 package guia2_poo_ej03.entidades;
 
