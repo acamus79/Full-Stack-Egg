7b8b88c31861ec78d49e56398a21156330c14441
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-19 12:17:09 -0300

    Primera Guia Java Extra 18

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_18.java
@@ -0,0 +1,43 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Escriba un programa que averigüe si dos vectores de N enteros son 
+    iguales (la comparación deberá detenerse en cuanto se detecte alguna
+    diferencia entre los elementos).
+
+ */
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_extra_18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] vectorA = { 1, 2, 3, 5, 6 };
+        int[] vectorB = { 1, 3, 3, 5, 67 };
+        int contador = 0;
+        for (int i = 0; i < 5; i++) {
+            if (vectorA[i] != vectorB[i]) {
+                System.out.println(vectorA[i] + " != " + vectorB[i]);
+                System.out.println("Los vectores no son iguales");
+                break;
+            } else {
+                System.out.println(vectorA[i] + " = " + vectorB[i]);
+                contador += 1;
+            }
+        }
+        if (contador == 5) {
+            System.out.println("Los vectores son iguales");
+        }
+    }
+        
+        
+}
