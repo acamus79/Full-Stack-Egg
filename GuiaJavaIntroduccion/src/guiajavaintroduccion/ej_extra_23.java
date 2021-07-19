6e0437d59ef12e518bf6ce1ea5f894613f9b95e5
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-19 15:25:35 -0300

    Primera Guia Java Ejercicio 23

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_23.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_23.java
@@ -5,10 +5,11 @@
     
     Realizar un programa que complete un vector con los N primeros números de 
     la sucesión de Fibonacci. Recordar que la sucesión de Fibonacci es la 
-    sucesión de los siguientes números:1, 1, 2, 3, 5, 8, 13, 21, 34, 
-    ...Donde  cada  uno  de  los  números  se  calcula  sumando  los  dos  
-    anteriores  a  él.  Por ejemplo:La sucesión del número 2 se calcula 
-    sumando (1+1)Análogamente, la sucesión del número 3 es (1+2),
+    sucesión de los siguientes números: 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+    
+    Donde  cada  uno  de  los  números  se  calcula  sumando  los  dos  
+    anteriores  a  él.  Por ejemplo: La sucesión del número 2 se calcula 
+    sumando (1+1) Análogamente, la sucesión del número 3 es (1+2),
     Y la del 5 es (2+3),Y así sucesivamente...La sucesión de Fibonacci 
     se puede formalizar de acuerdo a la siguiente fórmula: 
     
@@ -24,6 +25,8 @@
  */
 package guiajavaintroduccion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Adrian E. Camus
@@ -35,6 +38,37 @@ public class ej_extra_23 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int n, m, numero, fibonacci;
+        String fibo = "";
+        // n - 1 = n
+        // n - 2 = m
+        Scanner scan = new Scanner(System.in);
+        n = 1;
+        m = 1;
+        System.out.println("Ingrese hasta donde quiere imprimir");
+        numero = scan.nextInt();
+        for (int i = 0; i < numero; i++) {
+            if (i == 0) {
+                n = 1;
+                m = 0;
+                fibonacci = n + m;
+                m = n;
+                n = fibonacci;
+            } else {
+                fibonacci = n + m;
+                m = n;
+                n = fibonacci;
+            }
+            if (i < (numero - 1)) {
+                fibo += Integer.toString(fibonacci) + " + ";
+            } else {
+                fibo += Integer.toString(fibonacci);
+            }
+        }
+        System.out.println("Fibonacci:");
+        System.out.println(fibo);
+        scan.close();
+            
     }
     
 }
