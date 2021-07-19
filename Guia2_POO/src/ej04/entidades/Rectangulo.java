bd759a029963690aaf2597604f3091a18618d217
acamus79 <adriancamus1979@gmail.com>
2021-07-19 20:30:40 -0300

    Guia 2 Java POO Ejercicio 04

--- Guia2_POO/src/ej04/entidades/Rectangulo.java
+++ Guia2_POO/src/ej04/entidades/Rectangulo.java
@@ -10,5 +10,29 @@ package ej04.entidades;
  * @author Adrian E. Camus
  */
 public class Rectangulo {
+    private int base;
+    private int altura;
+
+    public Rectangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    
     
 }
