5bd9094bed495b72b43848d9d9b3927d81269683
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-19 11:23:01 -0300

    Primera Guia Java Extra 15

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_15.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_15.java
@@ -9,21 +9,57 @@
     al usuario si quiere seguir mostrando personas y frenar cuando el usuario 
     ingrese la palabra “No”.
 
-
  */
 package guiajavaintroduccion;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Adrian E. Camus
  */
 public class ej_extra_15 {
-
+    //funcion mostrar        
+    public static void persona(int n){
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        int[] edad = new int[n];
+        String[] nombres = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("Ingrese el nombre de la persona");
+            nombres[i] = leer.next();
+            System.out.println("Ingrese la edad");
+            edad[i] =leer.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println("Su nombre: " + nombres[i]);
+            System.out.println("Su edad: " + edad[i]);
+            if (i < (int) (n - 1)) {
+                System.out.println("Desea seguir mostrando personas? (Si/No)");
+                String confirma = leer.next();
+                if ("s".equals(confirma.substring(0, 1).toLowerCase())) {
+                    continue;
+                }
+                if ("n".equals(confirma.substring(0, 1).toLowerCase())) {
+                    break;
+                }
+            }
+        }
+        leer.close();
+    }
+        
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Ingrese la cantidad de personas");
+        int n = leer.nextInt();
+        persona(n);
+        leer.close();
+        
     }
     
 }
