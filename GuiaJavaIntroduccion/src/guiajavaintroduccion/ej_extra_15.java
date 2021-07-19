7bfa13dbba7d404ac883b78c4dec58c0b15bbb49
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-19 10:53:44 -0300

    Primera Guia Java Extra 15

---
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_15.java
@@ -0,0 +1,29 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Diseñe una función que pida el nombre y la edad de N personas e imprima 
+    los datos de las personas ingresadas por teclado e indique si son mayores 
+    o menores de edad. Después de cada persona, el programa debe preguntarle 
+    al usuario si quiere seguir mostrando personas y frenar cuando el usuario 
+    ingrese la palabra “No”.
+
+
+ */
+package guiajavaintroduccion;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ej_extra_15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
