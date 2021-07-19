ce83d4db43c8f40421c920de90416255cb4f10f2
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-19 15:10:13 -0300

    Primera Guia Java Ejercicio 25

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_25.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_25.java
@@ -12,52 +12,49 @@ package guiajavaintroduccion;
 /**
  *
  * @author Adrian E. Camus
- * 
+ *
  */
-    
-  public class ej_25 {
+public class ej_25 {
 
-      
     //Funcion llena con aleatorios una Matriz cuadrada de indice N 
-    public static int[][] llenaMatriz(int matrix[][],int n){
-       matrix= new int[n][n];
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j < n; j++) {
-                    matrix[i][j] = (int)(Math.random()*99+1); 
-                }
+    public static int[][] llenaMatriz(int matrix[][], int n) {
+        matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = (int) (Math.random() * 99 + 1);
             }
+        }
         return matrix;
-   }
-      
-     //Funcion para Mostrar una matriz cuadrada de indice N
-    public static void muestraMatriz (int matrix[][],int n){
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j < n; j++) {
-                    System.out.print("[" + matrix[i][j] + "]"); 
-                }
-                System.out.println(" ");
-            }
     }
+
+    //Funcion para Mostrar una matriz cuadrada de indice N
+    public static void muestraMatriz(int matrix[][], int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("[" + matrix[i][j] + "]");
+            }
+            System.out.println(" ");
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         int n = 4;
         int[][] matriz = new int[n][n];
 
-        matriz = llenaMatriz(matriz,n);
+        matriz = llenaMatriz(matriz, n);
         System.out.println("la matriz generada es:");
-        muestraMatriz(matriz,n);
+        muestraMatriz(matriz, n);
         System.out.println("Su transpuesta es:");
-        for (int i = 0; i < n; i++){
-               for (int j = 0; j < n; j++) {
-                    System.out.print("[" + matriz[j][i] + "]"); 
-                }
-                System.out.println(" ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("[" + matriz[j][i] + "]");
             }
+            System.out.println(" ");
+        }
     }
-    
+
 }
-
-
