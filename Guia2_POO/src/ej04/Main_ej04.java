33bbc317c2e6bbb3275002313df85d8f9c035d50
acamus79 <adriancamus1979@gmail.com>
2021-07-19 20:27:11 -0300

    Guia 2 Java POO Ejercicio 04

---
+++ Guia2_POO/src/ej04/Main_ej04.java
@@ -0,0 +1,34 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Crear una clase Rectángulo que modele rectángulos por medio de un atributo
+privado base y un atributo privado altura. La clase incluirá un método para 
+crear el rectángulo con los datos del Rectángulo dados por el usuario. También
+incluirá un método para calcular la superficie del rectángulo y un método para
+calcular el perímetro del rectángulo. Por último, tendremos un método que 
+dibujará el rectángulo mediante asteriscos usando la base y la altura. Se 
+deberán además definir los métodos getters, setters y constructores 
+correspondientes.
+
+Formulas: Superficie = base ∗ altura / Perímetro = (base + altura) ∗ 2.
+
+ */
+
+package ej04;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Main_ej04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
