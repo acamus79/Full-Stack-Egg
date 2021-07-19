4c594785bfc43171e2ea3603239393003d132ae8
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-19 14:20:28 -0300

    Primera Guia Java Extra 23

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_23.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_23.java
@@ -2,6 +2,25 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+    
+    Realizar un programa que complete un vector con los N primeros números de 
+    la sucesión de Fibonacci. Recordar que la sucesión de Fibonacci es la 
+    sucesión de los siguientes números:1, 1, 2, 3, 5, 8, 13, 21, 34, 
+    ...Donde  cada  uno  de  los  números  se  calcula  sumando  los  dos  
+    anteriores  a  él.  Por ejemplo:La sucesión del número 2 se calcula 
+    sumando (1+1)Análogamente, la sucesión del número 3 es (1+2),
+    Y la del 5 es (2+3),Y así sucesivamente...La sucesión de Fibonacci 
+    se puede formalizar de acuerdo a la siguiente fórmula: 
+    
+    Fibonacci n= Fibonacci n-1+ Fibonacci n-2 para todo n>1
+    Fibonacci n = 1 para todo n<=1
+    
+    Por  lo  tanto,  si  queremos  calcular  el  término  “n”  debemos  
+    escribir  una  función  que reciba como parámetro el valor de “n” y que 
+    calcule la serie hasta llegar a ese valor.Para  conocer  más  acerca  de  
+    la  serie  de  Fibonacci  consultar  el  siguiente 
+    link: https://quantdare.com/numeros-de-fibonacci/
+
  */
 package guiajavaintroduccion;
 
