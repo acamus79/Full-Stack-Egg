ec99481ab7d3aad91df218082082974a5447ede6
acamus79 <adriancamus1979@gmail.com>
2021-07-19 20:36:34 -0300

    Guia 2 Java POO Ejercicio 04

--- Guia2_POO/src/ej04/servicios/ServiciosRectangulo.java
+++ Guia2_POO/src/ej04/servicios/ServiciosRectangulo.java
@@ -5,10 +5,24 @@
  */
 package ej04.servicios;
 
+import ej04.entidades.Rectangulo;
+import java.util.Scanner;
+
 /**
  *
  * @author Adrian E. Camus
  */
 public class ServiciosRectangulo {
-    
+
+    Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    int base, altura;
+
+    public Rectangulo crearRectangulo() {
+        System.out.println("Ingresa la base del rectangulo");
+        base = leer.nextInt();
+        System.out.println("Ingresa la altura del rectangulo");
+        altura = leer.nextInt();
+        return new Rectangulo(base,altura);
+    }
+
 }
