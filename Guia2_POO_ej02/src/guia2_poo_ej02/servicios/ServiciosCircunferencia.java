08a434c55f730dd2cd42ade3a42bf61ed93d0ae2
acamus79 <adriancamus1979@gmail.com>
2021-07-19 18:31:52 -0300

    Guia 2 Java POO Ejercicio 02

---
+++ Guia2_POO_ej02/src/guia2_poo_ej02/servicios/ServiciosCircunferencia.java
@@ -0,0 +1,18 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+package guia2_poo_ej02.servicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ServiciosCircunferencia {
+    Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    
+    
+}
