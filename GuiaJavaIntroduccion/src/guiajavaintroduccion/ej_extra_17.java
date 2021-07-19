217a596abd7d25e09428ce98955040db6073a5cc
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-19 12:08:48 -0300

    Primera Guia Java Extra 17

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_17.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_17.java
@@ -2,8 +2,14 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Realizar un algoritmo que calcule la suma de todos los elementos 
+    de un vector de tamaño N, con los valores ingresados por el usuario.
+
  */
 package guiajavaintroduccion;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,6 +22,20 @@ public class ej_extra_17 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Ingrese la cantidad de numeros");
+        int n = leer.nextInt();
+        int suma = 0;
+        int[] vector = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Elemento "+i+1);
+            vector[i] = leer.nextInt();
+            suma += vector[i];
+        }
+        System.out.println("La suma es: " + suma);
+        leer.close();
+        
+        
     }
     
 }
