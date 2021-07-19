0067039155fbce0b3cffd40fbfa80c0d166acaef
acamus79 <adriancamus1979@gmail.com>
2021-07-19 17:01:41 -0300

    Guia 1 Java Extra 13

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_13.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_13.java
@@ -34,8 +34,10 @@ public class ej_extra_13 {
         System.out.println("Ingrese la cantidad de familias");
         familias = leer.nextInt();
         for (int i = 0; i < familias; i++) {
+            
             System.out.println("Ingrese la cantidad de hijos que tiene");
             hijos = leer.nextInt();
+            
             for (int j = 0; j < hijos; j++) {
                 System.out.println("Ingrese la edad de su hijo " + (int) (j + 1));
                 edad += leer.nextInt();
