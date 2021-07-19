6e60f5ab0d0b9175423c8ecf3676e36d54b530a3
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-19 14:26:08 -0300

    Primera Guia Java Extra 22

--- GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_22.java
+++ GuiaJavaIntroduccion/src/guiajavaintroduccion/ej_extra_22.java
@@ -2,6 +2,18 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+    Construya un programa que lea 5 palabras de mínimo 3 y hasta 5 caracteres 
+    y, a medida que el usuario las va ingresando, construya una “sopa de 
+    letras para niños” de tamaño de 20 x 20 caracteres. Las palabras se 
+    ubicarán todas en orden horizontal en una fila que será seleccionada 
+    de manera aleatoria. Una vez concluida la ubicación de las palabras, 
+    rellene los espacios no utilizados con un número aleatorio del 0 al 9. 
+    Finalmente imprima por pantalla la sopa de letras creada. 
+
+    Nota:  Para  resolver  el  ejercicio  deberá  investigar  cómo  se utilizan
+    las  siguientes funciones de Java substring(), Length() y Math.random(). 
+
  */
 package guiajavaintroduccion;
 
@@ -17,5 +29,5 @@ public class ej_extra_22 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
