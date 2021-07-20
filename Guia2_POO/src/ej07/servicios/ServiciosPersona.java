1712d1ca363a937b40868950d730c6f5bb03ec1d
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-20 12:47:56 -0300

    Guia 2 Java POO Ejercicio 07

--- Guia2_POO/src/ej07/servicios/ServiciosPersona.java
+++ Guia2_POO/src/ej07/servicios/ServiciosPersona.java
@@ -2,6 +2,23 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+• Metodo crearPersona(): el método crear persona, le pide los valores de los 
+atributos al usuario y después se le asignan a sus respectivos atributos para 
+llenar el objeto Persona. Además, comprueba que el sexo introducido sea 
+correcto, es decir, H, M ú O. Si no es correcto se deberá mostrar un mensaje
+
+• Método calcularIMC(): calculara si la persona está en su peso ideal (peso en
+kg/(altura^2 en mt2)). Si esta fórmula da por resultado un valor menor que 20, 
+la función devuelve un -1. Si la fórmula da por resultado un número entre 20 y 
+25 (incluidos), significa que el peso está por debajo de su peso ideal y la 
+función devuelve un 0. Finalmente, si el resultado de la fórmula es un valor 
+mayor que 25 significa que la persona tiene sobrepeso, y la función devuelve 
+un 1. Se recomienda hacer uso de constantes para devolver estos valores.
+
+• Método esMayorDeEdad(): indica si la persona es mayor de edad. La función
+devuelve un booleano.
+
  */
 
 package ej07.servicios;
@@ -12,4 +29,6 @@ package ej07.servicios;
  */
 public class ServiciosPersona {
 
+    
+    
 }
