0108ea27960fdcb46ef03688767a68c058a28144
acamus79 <adriancamus1979@gmail.com>
2021-07-19 22:23:49 -0300

    Guia 2 Java POO Ejercicio 05

--- Guia2_POO/src/ej05/servicios/ServiciosCuenta.java
+++ Guia2_POO/src/ej05/servicios/ServiciosCuenta.java
@@ -2,13 +2,35 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+• Metodo para crear un objeto Cuenta, pidiéndole los datos al usuario.
+• Método ingresar(double ingreso): el método recibe una cantidad de dinero a
+ingresar y se la sumara a saldo actual.
+• Método retirar(double retiro): el método recibe una cantidad de dinero a retirar y
+se la restará al saldo actual. Si la cuenta no tiene la cantidad de dinero a retirar, se
+pondrá el saldo actual en 0.
+• Método extraccionRapida(): le permitirá sacar solo un 20% de su saldo. Validar
+que el usuario no saque más del 20%.
+• Método consultarSaldo(): permitirá consultar el saldo disponible en la cuenta.
+
  */
 package ej05.servicios;
+
+import ej05.entidades.Cuenta;
+import java.util.Scanner;
 
 /**
  *
  * @author Adrian E. Camus
  */
 public class ServiciosCuenta {
+    Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    
+    public Cuenta crearCuenta(){
+        
+        return null;
+        
+    }
+    
     
 }
