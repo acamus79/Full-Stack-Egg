db910769a63655c5538b2bb063e149b9ad43eb46
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-20 10:24:13 -0300

    Guia 2 Java POO Ejercicio 05

---
+++ Guia2_POO/src/ej06/entidades/Cafetera.java
@@ -0,0 +1,15 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package ej06.entidades;
+
+/**
+ * 
+ * @author Adrian E. Camus
+ */
+public class Cafetera {
+
+}
