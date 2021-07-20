970f5eed022673333938cae5d3f53363dfecdaf2
acamus79 <adriancamus1979@gmail.com>
2021-07-19 22:19:53 -0300

    Guia 2 Java POO Ejercicio 05

--- Guia2_POO/src/ej05/entidades/Cuenta.java
+++ Guia2_POO/src/ej05/entidades/Cuenta.java
@@ -2,6 +2,12 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+Realizar una clase llamada Cuenta (bancaria) que debe tener como mínimo los
+atributos: numeroCuenta (entero), el DNI del cliente (entero largo), 
+el saldo actual. Las operaciones asociadas a dicha clase son:
+• Constructor por defecto y constructor con DNI, saldo, número de cuenta e interés.
+• Agregar los métodos getters y setters correspondientes
+
  */
 package ej05.entidades;
 
@@ -10,5 +16,51 @@ package ej05.entidades;
  * @author Adrian E. Camus
  */
 public class Cuenta {
-    
+
+    private int numeroCuenta;
+    private long dni;
+    private float saldoActual, interes;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(int numeroCuenta, long dni, float saldoActual, float interes) {
+        this.numeroCuenta = numeroCuenta;
+        this.dni = dni;
+        this.saldoActual = saldoActual;
+        this.interes = interes;
+    }
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public long getDni() {
+        return dni;
+    }
+
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    public float getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(float saldoActual) {
+        this.saldoActual = saldoActual;
+    }
+
+    public float getInteres() {
+        return interes;
+    }
+
+    public void setInteres(float interes) {
+        this.interes = interes;
+    }
+
 }
