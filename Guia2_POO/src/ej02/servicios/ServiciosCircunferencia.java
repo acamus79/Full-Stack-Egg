c1e5adcf0fbf4fdbf208a9a506394658d786f0d3
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-20 15:22:38 -0300

    Guia 2 Java POO Ejercicio 02

--- Guia2_POO/src/ej02/servicios/ServiciosCircunferencia.java
+++ Guia2_POO/src/ej02/servicios/ServiciosCircunferencia.java
@@ -15,21 +15,22 @@ import java.util.Scanner;
 public class ServiciosCircunferencia {
 
     Scanner leer = new Scanner(System.in).useDelimiter("\n");
-    double radio;
+    
     public Circunferencia crearCircunsferencia() {
         System.out.println("Ingresa el radio de la circunsferencia");
-        radio = leer.nextDouble();
-        Circunferencia c = new Circunferencia(radio);
-        return c;
+        double radio = leer.nextDouble();
+        return new Circunferencia(radio);
     }
 
-    public double calculaArea() {
+    public double calculaArea(Circunferencia c) {
+        double radio = c.getRadio();
         double area;
         area = Math.PI*radio;
         return area;
     }
     
-    public double calculaPerimetro() {
+    public double calculaPerimetro(Circunferencia c) {
+        double radio = c.getRadio();
         double perimetro;
         perimetro = (2*Math.PI)*radio;
         return perimetro;
