e50d657944bad9f62ed424e82ff6b912d3c49900
acamus79 <adriancamus1979@gmail.com>
2021-07-20 19:33:45 -0300

    Guia 2 Java POO Ejercicio 08

--- Guia2_POO/src/ej08/Main_ej08.java
+++ Guia2_POO/src/ej08/Main_ej08.java
@@ -2,6 +2,40 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Realizar una clase llamada Cadena que tenga como atributos una frase (de tipo
+de String) y su longitud. En el main se creará el objeto y se le pedirá al 
+usuario que ingrese una frase que puede ser una palabra o varias palabras 
+separadas por un espacio en blanco  y  a  través  de  los  métodos  set,  se  
+guardará  la  frase  y  la  longitud  de  manera automática según la longitud 
+de la frase ingresada. Deberá además implementar los siguientes métodos:  
+ 
+• Método mostrarVocales(), deberá contabilizar la cantidad de vocales que 
+tiene la frase ingresada.
+
+• Método invertirFrase(), deberá invertir la frase ingresada y mostrar la 
+frase invertida por pantalla. Por ejemplo: Entrada: "casa blanca", 
+Salida: "acnalb asac". 
+
+• Método  vecesRepetido(String  letra),  deberá  recibir  por  parámetro  un  
+carácter ingresado  por  el  usuario  y  contabilizar  cuántas  veces  se  
+repite  el  carácter  en  la frase, por ejemplo:  
+Entrada: frase = "casa blanca". Salida: El carácter 'a' se repite 4 veces.
+
+• Método compararLongitud(String frase), deberá comparar la longitud de la
+frase que compone la clase con otra nueva frase ingresada por el usuario. 
+
+• Método unirFrases(String frase), deberá unir la frase contenida en la clase 
+Cadena con  una nueva frase ingresada por el usuario y mostrar la frase 
+resultante. 
+
+• Método  reemplazar(String  letra),  deberá  reemplazar  todas  las  letras  
+“a”  que  se encuentren  en  la  frase,  por  algún  otro  carácter  
+seleccionado  por  el  usuario  y mostrar la frase resultante. 
+
+• Método contiene(String letra), deberá comprobar si la frase contiene una 
+letra que ingresa el usuario y devuelve verdadero si la contiene y falso si no. 
+
  */
 
 package ej08;
