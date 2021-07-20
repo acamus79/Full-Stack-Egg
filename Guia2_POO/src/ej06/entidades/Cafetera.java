ccbc811e294c40d0815843e72e47033684f603f1
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-20 10:41:43 -0300

    Guia 2 Java POO Ejercicio 05

--- Guia2_POO/src/ej06/entidades/Cafetera.java
+++ Guia2_POO/src/ej06/entidades/Cafetera.java
@@ -2,6 +2,15 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Desarrolle una clase Cafetera con los atributos
+capacidadMaxima (la cantidad máxima de café que puede contener la cafetera) y
+cantidadActual (la cantidad actual de café que hay en la cafetera).
+
+• Constructor predeterminado o vacío
+• Constructor con la capacidad máxima y la cantidad actual
+• Métodos getters y setters.
+
  */
 
 package ej06.entidades;
@@ -11,5 +20,32 @@ package ej06.entidades;
  * @author Adrian E. Camus
  */
 public class Cafetera {
+    
+    private int capacidadMaxima;
+    private int cantidadActual;
 
+    public Cafetera() {
+    }
+
+    public Cafetera(int capacidadMaxima, int cantidadActual) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.cantidadActual = cantidadActual;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public int getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public void setCantidadActual(int cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
+        
 }
