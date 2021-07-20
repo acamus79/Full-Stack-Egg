9e04357bf7a9536d62080990dc6b036e5f059888
acamus79 <adriancamus1979@gmail.com>
2021-07-19 22:12:47 -0300

    Guia 2 Java POO Ejercicio 05

---
+++ Guia2_POO/src/ej05/Main_ej05.java
@@ -0,0 +1,39 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Realizar una clase llamada Cuenta (bancaria) que debe tener como mínimo los
+atributos: numeroCuenta (entero), el DNI del cliente (entero largo), el saldo actual. Las
+operaciones asociadas a dicha clase son:
+• Constructor por defecto y constructor con DNI, saldo, número de cuenta e interés.
+• Agregar los métodos getters y setters correspondientes
+• Metodo para crear un objeto Cuenta, pidiéndole los datos al usuario.
+• Método ingresar(double ingreso): el método recibe una cantidad de dinero a
+ingresar y se la sumara a saldo actual.
+• Método retirar(double retiro): el método recibe una cantidad de dinero a retirar y
+se la restará al saldo actual. Si la cuenta no tiene la cantidad de dinero a retirar, se
+pondrá el saldo actual en 0.
+• Método extraccionRapida(): le permitirá sacar solo un 20% de su saldo. Validar
+que el usuario no saque más del 20%.
+• Método consultarSaldo(): permitirá consultar el saldo disponible en la cuenta.
+• Método consultarDatos(): permitirá mostrar todos los datos de la cuenta
+
+ */
+
+package ej05;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Main_ej05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
