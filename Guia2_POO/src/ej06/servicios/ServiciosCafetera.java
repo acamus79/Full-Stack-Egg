450fdc1a0847fe09a3b096c8ea4863e2aa56cdba
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-20 10:43:37 -0300

    Guia 2 Java POO Ejercicio 05

--- Guia2_POO/src/ej06/servicios/ServiciosCafetera.java
+++ Guia2_POO/src/ej06/servicios/ServiciosCafetera.java
@@ -2,6 +2,18 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+• Método llenarCafetera(): hace que la cantidad actual sea igual a la capacidad
+máxima. 
+• Método servirTaza(int): se pide el tamaño de una taza vacía, el método recibe 
+el tamaño de la taza y simula la acción de servir la taza con la capacidad 
+indicada. Si la cantidad actual de café “no alcanza” para llenar la taza, se 
+sirve lo que quede. El método le informará al usuario si se llenó o no la taza,
+y de no haberse llenado en cuanto quedó la taza.
+• Método vaciarCafetera(): pone la cantidad de café actual en cero. 
+• Método agregarCafe(int): se le pide al usuario una cantidad de café, el 
+método lo recibe y se añade a la cafetera la cantidad de café indicada.
+
  */
 
 package ej06.servicios;
