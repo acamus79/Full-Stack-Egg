24980564cf2a66cc1edbb9b11aa5da50ed8ee229
acamus79 <adriancamus1979@gmail.com>
2021-07-21 15:54:55 -0300

    Guia 2 Java POO Ejercicio 09

--- Guia2_POO/src/ej09/entidades/Matematica.java
+++ Guia2_POO/src/ej09/entidades/Matematica.java
@@ -10,5 +10,39 @@ package ej09.entidades;
  * @author Adrian E. Camus
  */
 public class Matematica {
+    private double numero1;
+    private double numero2;
+
+    public Matematica() {
+    }
+
+    public Matematica(double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    @Override
+    public String toString() {
+        return "Numeros:\n" 
+                + "Primer numero: " + numero1 
+                + "\nSegundo numero: " + numero2;
+    }
+    
     
 }
