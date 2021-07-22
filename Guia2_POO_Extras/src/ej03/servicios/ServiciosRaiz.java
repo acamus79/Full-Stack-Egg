3bb055cd479ea0068b6ad6f44d9a274012e7474a
acamus79 <adriancamus1979@gmail.com>
2021-07-22 18:42:22 -0300

    Guia 2 Java POO Extra 03

---
+++ Guia2_POO_Extras/src/ej03/servicios/ServiciosRaiz.java
@@ -0,0 +1,14 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+package ej03.servicios;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ServiciosRaiz {
+    
+}
