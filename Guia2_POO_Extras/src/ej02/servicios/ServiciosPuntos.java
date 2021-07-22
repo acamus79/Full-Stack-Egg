0c31950bb98d733f900877165104baa8fc11ee21
acamus79 <adriancamus1979@gmail.com>
2021-07-22 17:59:35 -0300

    Guia 2 Java POO Extra 02

--- Guia2_POO_Extras/src/ej02/servicios/ServiciosPuntos.java
+++ Guia2_POO_Extras/src/ej02/servicios/ServiciosPuntos.java
@@ -5,10 +5,28 @@
  */
 package ej02.servicios;
 
+import ej02.entidades.Puntos;
+import java.util.Scanner;
+
 /**
  *
  * @author Adrian E. Camus
  */
 public class ServiciosPuntos {
+    Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    
+    public Puntos crearPuntos(){
+        System.out.println("Ingrese x1");
+        int x1 = leer.nextInt();
+        System.out.println("Ingrese y1");
+        int y1 = leer.nextInt();
+        System.out.println("Ingrese x2");
+        int x2 = leer.nextInt();
+        System.out.println("Ingrese y2");
+        int y2 = leer.nextInt();
+        
+        return new Puntos(x1,y1,x2,y2);
+    }
+    
     
 }
