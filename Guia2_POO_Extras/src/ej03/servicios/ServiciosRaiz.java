0805a1a1a49a1fedd6e71f15264340e1b1e204cf
acamus79 <adriancamus1979@gmail.com>
2021-07-22 20:09:08 -0300

    Guia 2 Java POO Extra 03

--- Guia2_POO_Extras/src/ej03/servicios/ServiciosRaiz.java
+++ Guia2_POO_Extras/src/ej03/servicios/ServiciosRaiz.java
@@ -5,10 +5,32 @@
  */
 package ej03.servicios;
 
+import ej03.entidades.Raiz;
+import java.util.Scanner;
+
 /**
  *
  * @author Adrian E. Camus
  */
 public class ServiciosRaiz {
+
+    Scanner leer = new Scanner(System.in).useDelimiter("\n");
+
+    public Raiz crearRaiz() {
+        System.out.println("Ingresa valor de a");
+        int a = leer.nextInt();
+        System.out.println("Ingresa valor de b");
+        int b = leer.nextInt();
+        System.out.println("Ingresa valor de c");
+        int c = leer.nextInt();
+        return new Raiz(a, b, c);
+
+    }
+    
+    
+
+    
+    
+    
     
 }
