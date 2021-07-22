754995d102226aabf37847829da39cd1be7866d5
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-22 13:36:14 -0300

    Guia 2 Java POO Ejercicio 11

---
+++ Guia2_POO/src/ej11/Main_ej11.java
@@ -0,0 +1,38 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Pongamos de lado un momento el concepto de POO, ahora vamos a trabajar solo con
+la clase Date. En este ejercicio deberemos instanciar en el main, una fecha 
+usando la clase Date, para esto vamos a tener que crear 3 variables, dia, mes 
+y anio que se le pedirán al usuario para poner el constructor del objeto Date. 
+Una vez creada la fecha de tipo Date, deberemos mostrarla y mostrar cuantos 
+años hay entre esa fecha y la fecha actual, que se puede conseguir instanciando
+un objeto Date con constructor vacío.
+
+Ejemplo fecha: Date fecha = new Date(anio, mes, dia);
+
+Ejemplo fecha actual: Date fechaActual = new Date();
+
+ */
+package ej11;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Main_ej11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+    }
+    
+}
