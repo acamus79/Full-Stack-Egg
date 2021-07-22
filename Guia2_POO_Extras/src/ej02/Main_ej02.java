428c4be4695b4ead1b2b426a6915950b2c2048fc
acamus79 <adriancamus1979@gmail.com>
2021-07-22 17:49:47 -0300

    Guia 2 Java POO Extra 02

---
+++ Guia2_POO_Extras/src/ej02/Main_ej02.java
@@ -0,0 +1,32 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Definir una clase llamada Puntos que contendrá las coordenadas de dos puntos, sus
+atributos serán, x1, y1, x2, y2, siendo cada x e y un punto. Generar un objeto puntos
+usando un método crearPuntos() que le pide al usuario los dos números y los ingresa
+en los atributos del objeto. Después, a través de otro método calcular y devolver la
+distancia que existe entre los dos puntos que existen en la clase Puntos. Para conocer
+como calcular la distancia entre dos puntos consulte el siguiente link:
+
+http://www.matematicatuya.com/GRAFICAecuaciones/S1a.html.
+
+ */
+
+package ej02;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Main_ej02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
