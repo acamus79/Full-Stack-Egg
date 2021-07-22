028e4852238a3ee43241c6d1d3f09008f629ee96
acamus79 <adriancamus1979@gmail.com>
2021-07-21 22:02:06 -0300

    Guia 2 Java POO Ejercicio 10

---
+++ Guia2_POO/src/ej10/Main_ej10.java
@@ -0,0 +1,32 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Realizar un programa en Java donde se creen dos arreglos: el primero 
+será un arreglo A de 50 números reales, y el segundo B, un arreglo de 20 
+números, también reales. El programa  deberá  inicializar  el  arreglo  A  
+con  números  aleatorios  y  mostrarlo  por pantalla. Luego, el arreglo A 
+se debe ordenar de menor a mayor y copiar los primeros 10  números  ordenados 
+al  arreglo  B  de  20  elementos,  y  rellenar  los  10  últimos elementos  
+con  el  valor  0.5.  Mostrar  los  dos  arreglos  resultantes:  el  ordenado 
+de  50 elementos y el combinado de 20. 
+
+ */
+
+package ej10;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Main_ej10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
