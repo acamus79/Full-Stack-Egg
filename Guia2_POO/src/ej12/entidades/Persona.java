24cb68505ef367026363cfd4b62a4a4d15510f3f
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-22 15:54:08 -0300

    Guia 2 Java POO Ejercicio 12

--- Guia2_POO/src/ej12/entidades/Persona.java
+++ Guia2_POO/src/ej12/entidades/Persona.java
@@ -16,6 +16,30 @@ public class Persona {
 
     private String nombre;
     private Date fechaNac;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, Date fechaNac) {
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
     
     
     
