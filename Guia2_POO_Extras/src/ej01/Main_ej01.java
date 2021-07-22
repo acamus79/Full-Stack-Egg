3db2e84a6b084e9a5426d1a52cfa088d40f0fc69
acamus79 <adriancamus1979@gmail.com>
2021-07-21 21:59:05 -0300

    Guia 2 Java POO Extras Ejercicio 01

--- Guia2_POO_Extras/src/ej01/Main_ej01.java
+++ Guia2_POO_Extras/src/ej01/Main_ej01.java
@@ -2,6 +2,13 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Desarrollar  una  clase  Cancion  con  los  siguientes  atributos:  titulo  
+y  autor.  Se  deberá definir además dos constructores: uno vacío que 
+inicializa el titulo y el autor a cadenas vacías y otro que reciba como 
+parámetros el titulo y el autor de la canción. Se deberán además definir 
+los métodos getters y setters correspondientes. 
+
  */
 
 package ej01;
