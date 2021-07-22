e4baaf3ad83a6dc7f7339dce2d5ba3ef647e1c87
acamus79 <adriancamus1979@gmail.com>
2021-07-21 21:55:25 -0300

    Guia 2 Java POO Extras

---
+++ Guia2_POO_Extras/src/ej01/Main_ej01.java
@@ -0,0 +1,22 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package ej01;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Main_ej01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
