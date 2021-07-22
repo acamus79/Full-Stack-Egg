0cf1e12b934e653bac4420a236eb5a2040dff1c1
acamus79 <adriancamus1979@gmail.com>
2021-07-22 19:38:11 -0300

    Guia 2 Java POO Extra 03

--- Guia2_POO_Extras/src/ej03/entidades/Raiz.java
+++ Guia2_POO_Extras/src/ej03/entidades/Raiz.java
@@ -10,5 +10,42 @@ package ej03.entidades;
  * @author Adrian E. Camus
  */
 public class Raiz {
+    private int a;
+    private int b;
+    private int c;
+
+    public Raiz() {
+    }
+
+    public Raiz(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+    
     
 }
