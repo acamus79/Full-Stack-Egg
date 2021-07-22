282a3171e7970a9f5a1cdee335e43797248174e9
acamus79 <adriancamus1979@gmail.com>
2021-07-22 18:29:44 -0300

    Guia 2 Java POO Extra 03

--- Guia2_POO_Extras/src/ej03/Main_ej03.java
+++ Guia2_POO_Extras/src/ej03/Main_ej03.java
@@ -2,6 +2,29 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Vamos a realizar una clase llamada Raices, donde representaremos los valores de una
+ecuación de 2o grado. Tendremos los 3 coeficientes como atributos, llamémosles a, b
+y c. Hay que insertar estos 3 valores para construir el objeto a través de un método
+constructor. Luego, las operaciones que se podrán realizar son las siguientes:
+• Método getDiscriminante(): devuelve el valor del discriminante (double). El 
+discriminante tiene la siguiente formula: (b^2)-4*a*c 
+• Método tieneRaices(): devuelve un booleano indicando si tiene dos soluciones, para 
+que esto ocurra, el discriminante debe ser mayor o igual que 0. 
+• Método tieneRaiz(): devuelve un booleano indicando si tiene una única solución, 
+para que esto ocurra, el discriminante debe ser igual que 0. 
+• Método obtenerRaices(): llama a tieneRaíces() y si devolvió́true, imprime las 2 
+posibles soluciones. 
+• Método obtenerRaiz(): llama a tieneRaiz() y si devolvió́true imprime una única raíz. 
+Es en el caso en que se tenga una única solución posible. 
+• Método calcular(): esté método llamará tieneRaices() y a tieneRaíz(), y mostrará por 
+pantalla las posibles soluciones que tiene nuestra ecuación con los métodos 
+obtenerRaices() o obtenerRaiz(), según lo que devuelvan nuestros métodos y en 
+caso de no existir solución, se mostrará un mensaje. 
+Nota: Formula ecuación 2o grado: (-b±√((b^2)-(4*a*c)))/(2*a) Solo varia el signo 
+delante de -b
+
+
  */
 
 package ej03;
