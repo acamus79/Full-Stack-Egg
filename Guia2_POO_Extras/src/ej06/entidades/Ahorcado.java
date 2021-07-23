5aaf15183cc67254810dd2c1dd414565b971ff48
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-23 13:11:17 -0300

    Guia 2 Java POO Extra 05

--- Guia2_POO_Extras/src/ej06/entidades/Ahorcado.java
+++ Guia2_POO_Extras/src/ej06/entidades/Ahorcado.java
@@ -11,5 +11,16 @@ package ej06.entidades;
  * @author Adrian E. Camus
  */
 public class Ahorcado {
+    
+    private final String palabra[] = {"electroencefalograma","psicofarmaco","prohibido","atencion","","credencial","coronavirus","vacuna","abogado","cirujano","noviembre","abril"};
+    
+
+    public Ahorcado() {
+        
+    }
+
+    public String getPalabra(int i) {
+        return palabra[i];
+    }
 
 }
