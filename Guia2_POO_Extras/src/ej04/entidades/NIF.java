c3e48941b08c87aa71ddb608d99119fbd2df1ecd
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-23 09:52:13 -0300

    Guia 2 Java POO Extra 04

--- Guia2_POO_Extras/src/ej04/entidades/NIF.java
+++ Guia2_POO_Extras/src/ej04/entidades/NIF.java
@@ -2,14 +2,45 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
- */
 
+Dígito Verificador. Crear una clase NIF que se usará para mantener DNIs 
+con su correspondiente letra (NIF). Los atributos serán el número de DNI 
+(entero largo) y la letra (String o char) que le corresponde. 
+Dispondrá de los siguientes métodos: 
+
+• Métodos getters y setters para el número de DNI y la letra.
+
+ */
 package ej04.entidades;
 
 /**
- * 
+ *
  * @author Adrian E. Camus
  */
 public class NIF {
+
+    private long dnis;
+    private String letraNIF;
+
+    public long getDnis() {
+        return dnis;
+    }
+
+    public void setDnis(long dnis) {
+        this.dnis = dnis;
+    }
+
+    public String getLetraNIF() {
+        return letraNIF;
+    }
+
+    public void setLetraNIF(String letraNIF) {
+        this.letraNIF = letraNIF;
+    }
+
+    @Override
+    public String toString() {
+        return "NIF: " + dnis + "-" + letraNIF;
+    }
 
 }
