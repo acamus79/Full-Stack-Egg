548c6cf7551f71329a40d58bc472c61b615e7795
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-23 12:12:31 -0300

    Guia 2 Java POO Extra 05

--- Guia2_POO_Extras/src/ej05/entidades/Calendario.java
+++ Guia2_POO_Extras/src/ej05/entidades/Calendario.java
@@ -11,5 +11,18 @@ package ej05.entidades;
  * @author Adrian E. Camus
  */
 public class Calendario {
+//"final" porque seria como una constante para que no se modifique
+    private final String calendario[] = {"enero","febrero","marzo","abril","mayo","junio","julio","agosto","setiembre","octubre","noviembre","diciembre"};
+    
 
+    public Calendario() {
+        
+    }
+
+    public String getCalendario(int i) {
+        return calendario[i];
+    }
+
+    
+    
 }
