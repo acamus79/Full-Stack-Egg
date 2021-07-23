723ced68fac9618f96a175a4c0141b97f4216f73
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-23 12:39:50 -0300

    Guia 2 Java POO Extra 05

--- Guia2_POO_Extras/src/ej05/servicios/ServiciosCalendario.java
+++ Guia2_POO_Extras/src/ej05/servicios/ServiciosCalendario.java
@@ -6,10 +6,29 @@
 
 package ej05.servicios;
 
+import ej05.entidades.Calendario;
+import java.util.Scanner;
+
 /**
  * 
  * @author Adrian E. Camus
  */
 public class ServiciosCalendario {
-
+Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    public void adivinaCalendario(){
+        Calendario c = new Calendario();
+        int indice = (int) (Math.random() * 12);
+        
+        System.out.println("Adivine el mes secreto. \n" +
+        "Introduzca el nombre del mes en minúsculas: ");
+        String adivina = leer.next().toLowerCase();
+        
+        while (!adivina.equals(c.getCalendario(indice))){
+            System.out.println("No ha acertado. Intente adivinarlo introduciendo otro mes: ");
+            adivina = leer.next().toLowerCase();
+        }
+        if (adivina.equals(c.getCalendario(indice))){
+            System.out.println("¡Ha acertado!");
+        }
+    }
 }
