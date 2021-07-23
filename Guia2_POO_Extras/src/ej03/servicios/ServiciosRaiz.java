a5776832cfceb7070ef08d117f6ca46eb38a8574
acamus79 <adriancamus1979@gmail.com>
2021-07-22 23:07:54 -0300

    Guia 2 Java POO Extra 03

--- Guia2_POO_Extras/src/ej03/servicios/ServiciosRaiz.java
+++ Guia2_POO_Extras/src/ej03/servicios/ServiciosRaiz.java
@@ -3,14 +3,12 @@
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
 
-• Método tieneRaices(): devuelve un booleano indicando si tiene dos soluciones, para 
-que esto ocurra, el discriminante debe ser mayor o igual que 0. 
-• Método tieneRaiz(): devuelve un booleano indicando si tiene una única solución, 
-para que esto ocurra, el discriminante debe ser igual que 0. 
-• Método obtenerRaices(): llama a tieneRaíces() y si devolvió́true, imprime las 2 
-posibles soluciones. 
+ 
+
+
 • Método obtenerRaiz(): llama a tieneRaiz() y si devolvió́true imprime una única raíz. 
 Es en el caso en que se tenga una única solución posible. 
+
 • Método calcular(): esté método llamará tieneRaices() y a tieneRaíz(), y mostrará por 
 pantalla las posibles soluciones que tiene nuestra ecuación con los métodos 
 obtenerRaices() o obtenerRaiz(), según lo que devuelvan nuestros métodos y en 
@@ -36,19 +34,57 @@ public class ServiciosRaiz {
 
     public Raiz crearRaiz() {
         System.out.println("Ingresa valor de a");
-        int a = leer.nextInt();
+        double a = leer.nextInt();
         System.out.println("Ingresa valor de b");
-        int b = leer.nextInt();
+        double b = leer.nextInt();
         System.out.println("Ingresa valor de c");
-        int c = leer.nextInt();
+        double c = leer.nextInt();
         return new Raiz(a, b, c);
 
     }
+    /*
+Método tieneRaices(): devuelve un booleano indicando si tiene dos soluciones, 
+para que esto ocurra, el discriminante debe ser mayor o igual que 0.
+    */
+    private static boolean tieneRaices(Raiz r){
+//        if(r.getDiscriminante()>=0){
+//            return true;
+//        }else{
+//            return false;
+//        }
+        return r.getDiscriminante()>0;
+    }
     
+/*
+Método tieneRaiz(): devuelve un booleano indicando si tiene una única solución, 
+para que esto ocurra, el discriminante debe ser igual que 0.     
+*/            
+    private static boolean tieneRaiz(Raiz r){
+        return r.getDiscriminante()==0;
+//        if(r.getDiscriminante()==0){
+//            return true;
+//        }else {
+//            return false;
+//        }
+        
+    }
     
-
-    
-    
+/*
+Método obtenerRaices(): llama a tieneRaices() y si devolvió true, imprime las 2 
+posibles soluciones. 
+*/    
+  
+    private static void obtenerRaices(Raiz r){
+        //(-b±√((b^2)-(4*a*c)))/(2*a)
+        if (tieneRaices(r)==true){
+            System.out.println("La ecuacion tiene dos soluciones");
+            double res1 = (-r.getB())+(Math.sqrt(r.getDiscriminante()))/2*r.getA();
+            double res2 = (-r.getB())-(Math.sqrt(r.getDiscriminante()))/2*r.getA();
+            System.out.println("Primera solucion: "+res1);
+            System.out.println("Segunda solucion: "+res2);
+        }
+        
+    }
     
     
 }
