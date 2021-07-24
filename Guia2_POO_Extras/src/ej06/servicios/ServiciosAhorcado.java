aa04e348615d27d792506a30e17d64bcb7d900e7
acamus79 <adriancamus1979@gmail.com>
2021-07-24 20:49:49 -0300

    Guia 2 Java POO Extra 06

--- Guia2_POO_Extras/src/ej06/servicios/ServiciosAhorcado.java
+++ Guia2_POO_Extras/src/ej06/servicios/ServiciosAhorcado.java
@@ -28,13 +28,8 @@ encontradas en 0.
         String palabra = leer.next();
         System.out.println("Ingresa la cantidad de jugadas maximas");
         int cantJug = leer.nextInt();
-        String[] vector = new String[palabra.length()];
 
-        for (int i = 0; i < palabra.length(); i++) {
-            vector[i] = palabra.substring(i);
-        }
-
-        return new Ahorcado(vector, 0, cantJug);
+        return new Ahorcado(palabra, 0, cantJug);
     }
 
     /*
@@ -48,9 +43,9 @@ Nota: buscar como se usa el vector.length.*/
 Método buscar(letra): este método recibe una letra dada por el usuario y 
 busca si la letra ingresada es parte de la palabra o no. También informará si 
 la letra estaba o no.*/
-    private static boolean buscarLetra(Ahorcado a, String l) {
-        for (String palabra : a.getPalabra()) {
-            if (l.equals(palabra)) {
+    private static boolean buscarLetra(Ahorcado a, char letra) {
+        for (int i = 0; i < a.getPalabra().length; i++) {
+            if (a.getPalabra()[i] == letra) {
                 return true;
             }
         }
@@ -68,12 +63,12 @@ sus oportunidades.*/
         int cuentaLetra = 0;
         int oportunidades = a.getJugadas();
         System.out.println("Ingresa la letra a buscar");
-        String letra = leer.next().toLowerCase().substring(0, 0);
+        char letra = leer.next().toLowerCase().charAt(0);
 
         if (buscarLetra(a, letra)) {
             System.out.println("La letra SI esta en la palabra");
-            for (int i = 0; i < a.getPalabra().length; i++) {
-                if (letra.equals(a.getPalabra()[i])) {
+            for (int i = 0; i < a.getPalabra().size(); i++) {
+                if (letra == a.getPalabra().elementAt(i).toString().charAt(0)) {
                     cuentaLetra++;
                 }
             }
@@ -101,7 +96,7 @@ o se quede sin intentos. Este método se llamará en el main.*/
             encontradas(a);
             longitud(a);
             intentos(a);
-            if (a.getJugadas() > 1 && a.getLetrasEncontradas() == a.getPalabra().length) {
+            if (a.getJugadas() > 1 && a.getLetrasEncontradas() == a.getPalabra().size()) {
                 System.out.println("MUY BIEN Ganaste!");
                 break;
             }
