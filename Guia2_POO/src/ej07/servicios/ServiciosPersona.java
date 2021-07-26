5b75e6a4dbe60ed03879101c62840b63e37105ab
acamus79 <adriancamus1979@gmail.com>
2021-07-26 13:39:27 -0300

    Update ServiciosPersona.java

--- Guia2_POO/src/ej07/servicios/ServiciosPersona.java
+++ Guia2_POO/src/ej07/servicios/ServiciosPersona.java
@@ -2,23 +2,6 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
-
-• Metodo crearPersona(): el método crear persona, le pide los valores de los 
-atributos al usuario y después se le asignan a sus respectivos atributos para 
-llenar el objeto Persona. Además, comprueba que el sexo introducido sea 
-correcto, es decir, H, M ú O. Si no es correcto se deberá mostrar un mensaje
-
-• Método calcularIMC(): calculara si la persona está en su peso ideal (peso en
-kg/(altura^2 en mt2)). Si esta fórmula da por resultado un valor menor que 20, 
-la función devuelve un -1. Si la fórmula da por resultado un número entre 20 y 
-25 (incluidos), significa que el peso está por debajo de su peso ideal y la 
-función devuelve un 0. Finalmente, si el resultado de la fórmula es un valor 
-mayor que 25 significa que la persona tiene sobrepeso, y la función devuelve 
-un 1. Se recomienda hacer uso de constantes para devolver estos valores.
-
-• Método esMayorDeEdad(): indica si la persona es mayor de edad. La función
-devuelve un booleano.
-
  */
 package ej07.servicios;
 
@@ -30,11 +13,19 @@ import java.util.Scanner;
  * @author Adrian E. Camus
  */
 public class ServiciosPersona {
+    //creo variables locales que me van a servir para el metodo estadistica
     int pi=0,bp=0,sp=0,contMayor=0, contMenor=0;
-    Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    
+/*
+Metodo crearPersona(): el método crear persona, le pide los valores de los 
+atributos al usuario y después se le asignan a sus respectivos atributos para 
+llenar el objeto Persona. Además, comprueba que el sexo introducido sea 
+correcto, es decir, H, M ú O. Si no es correcto se deberá mostrar un mensaje
 
-    //(String nombre, int edad, String sexo, float peso, float altura)
+(String nombre, int edad, String sexo, float peso, float altura)
+*/
     public Persona crearPersona() {
+    Scanner leer = new Scanner(System.in).useDelimiter("\n");
         boolean bandera = true;
         String sexo;
         System.out.println("Ingrese el Nombre de la Persona");
@@ -59,14 +50,22 @@ public class ServiciosPersona {
             }
 
         } while (!bandera);
-
+        leer.close;
         System.out.println("Ingrese el peso de la Persona");
         float p = leer.nextFloat();
         System.out.println("Ingrese la altura de la Persona");
         double al = leer.nextDouble();
         return new Persona(nom,edad,sexo,p,al);
     }
-
+/*
+Método calcularIMC(): calculara si la persona está en su peso ideal (peso en
+kg/(altura^2 en mt2)). Si esta fórmula da por resultado un valor menor que 20, 
+la función devuelve un -1. Si la fórmula da por resultado un número entre 20 y 
+25 (incluidos), significa que el peso está por debajo de su peso ideal y la 
+función devuelve un 0. Finalmente, si el resultado de la fórmula es un valor 
+mayor que 25 significa que la persona tiene sobrepeso, y la función devuelve 
+un 1. Se recomienda hacer uso de constantes para devolver estos valores.
+*/
     public int calcularIMC(Persona p) {
         int imc = 0;
 
@@ -86,7 +85,10 @@ public class ServiciosPersona {
 
         return imc;
     }
-
+/*
+Método esMayorDeEdad(): indica si la persona es mayor de edad. La función
+devuelve un booleano.
+*/
     public boolean esMayorDeEdad(Persona p) {
         boolean mayor = false;
         if (p.getEdad() > 18) {
@@ -111,8 +113,8 @@ public class ServiciosPersona {
         System.out.println("***********************************");
         System.out.println("**           PORCENTAJES         **");
         System.out.println("***********************************");
-        System.out.println("El % de personas Mayores es "+(contMayor*100)/cantPersonas+"%");
-        System.out.println("El % de personas Menores es "+(contMenor*100)/cantPersonas+"%");
+        System.out.println("El % de personas Mayores de edad es "+(contMayor*100)/cantPersonas+"%");
+        System.out.println("El % de personas Menores de edad es "+(contMenor*100)/cantPersonas+"%");
         System.out.println("El % de personas con peso ideal es "+(pi*100)/cantPersonas+"%");
         System.out.println("El % de personas con sobrepeso es "+(sp*100)/cantPersonas+"%");
         System.out.println("El % de personas con bajo peso es "+(bp*100)/cantPersonas+"%");
