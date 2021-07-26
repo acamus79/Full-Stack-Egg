11598ff35b97ae3aae27cb44a14fa0d276c79a06
acamus79 <CASI2DO@DESKTOP-0SVO84P.minseg.gov.ar>
2021-07-26 14:15:23 -0300

    Guia 2 Java POO Extra 02

--- Guia2_POO_Extras/src/ej02/Main_punto_InterpretacionPropia.java
+++ Guia2_POO_Extras/src/ej02/Main_punto_InterpretacionPropia.java
@@ -2,6 +2,12 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Mismo enunciado interpretacion propia
+discusion foro:
+
+https://moodle.eggeducacion.app/mod/forum/discuss.php?d=2198#p8945
+
  */
 package ej02;
 
