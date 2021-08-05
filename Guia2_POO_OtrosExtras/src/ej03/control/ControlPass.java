e124f74a33fc4ecea08cd8be4cb1872c2f17203c
acamus79 <adriancamus1979@gmail.com>
2021-08-04 21:28:38 -0300

    Guia Poo Otros Extras

---
+++ Guia2_POO_OtrosExtras/src/ej03/control/ControlPass.java
@@ -0,0 +1,46 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+Métodos para desarrollar:
+ Ingresar datos de usuarios (usando el constructor. HACER INGRESO DE DATOS FIJOS (no
+por teclado)
+
+ Crear un método para ingresar una contraseña (crearPass). En este método, debemos
+validar que la longitud sea correcta. (10). En caso de ser correcto, almaceno la contraseña.
+Si ingreso a esta opción del menú, indefectiblemente deberá dejar su contraseña asignada.
+
+ Crear un método para analizar la contraseña(analizarPass). Donde:
+o SI Existe al menos una letra z : Es nivel MEDIO
+o Si Existe al menos una letra z y al menos 2 letras a: Es nivel ALTO
+o Si ninguna condición se cumple es nivel BAJO
+
+ Crear los métodos correspondientes para modificar el nombre o DNI. Atención! Primero
+debe ingresar el pass para poder realizar dicha gestión. Caso contrario se impedirá el
+acceso a modificar los datos.
+
+ A través de un menú de opciones:
+o A) ingresar la contraseña, recordar que antes de guardarla debe ser ingresada una
+contraseña valida).
+o B) Mensaje al usuario que tipo de NIVEL es su contraseña
+o C) Modificar contraseña, donde primero debe poner su contraseña anterior para
+dar permiso
+o C) Modificar nombre, donde primero debe poner su contraseña para dar permiso,
+SINO impedir cambios
+o D) Modificar DNI, donde primero debe poner su contraseña para dar permiso,
+SINO impedir cambios
+
+
+ */
+package ej03.control;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class ControlPass {
+    
+    
+    
+    
+}
