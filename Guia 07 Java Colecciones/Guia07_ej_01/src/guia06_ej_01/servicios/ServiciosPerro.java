40731dbf775654dc44f73b5786d67cf6bdba84f5
acamus79 <adriancamus1979@gmail.com>
2021-08-09 22:01:17 -0300

    Guia 07 Collections Ejercicio 01

--- Guia 07 Java Colecciones/Guia07_ej_01/src/guia06_ej_01/servicios/ServiciosPerro.java
+++ Guia 07 Java Colecciones/Guia07_ej_01/src/guia06_ej_01/servicios/ServiciosPerro.java
@@ -5,10 +5,40 @@
  */
 package guia06_ej_01.servicios;
 
+import guia06_ej_01.entidades.Perro;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Adrian E. Camus
  */
 public class ServiciosPerro {
+    private Scanner leer;
+    private ArrayList perrosList;
+
+    public ServiciosPerro() {
+        this.leer = new Scanner(System.in).useDelimiter("\n");
+        this.perrosList = new ArrayList();
+    }
+    
+    /**
+     * Crea y añade a mi arraylist un objetop Perro
+     * @return un objeto de la clase perro 
+     */
+    public Perro creaPerro(){
+        System.out.println("Ingrese la Raza del Perro");
+        String laraza = leer.next();
+        Perro p = new Perro(laraza);
+        perrosList.add(p);
+        return p;
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
--- Guia 07 Java Colecciones/Guia07_ej_01/src/guia07_ej_01_02/Guia06_ej_01.java
+++ Guia 07 Java Colecciones/Guia07_ej_01/src/guia07_ej_01_02/Guia06_ej_01.java
@@ -3,17 +3,22 @@
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
 
-Diseñar un programa que lea y guarde razas de perros en un ArrayList de tipo
+01. Diseñar un programa que lea y guarde razas de perros en un ArrayList de tipo
 String. El programa pedirá una raza de perro en un bucle, el mismo se guardará
 en la lista y después se le preguntará al usuario si quiere guardar otro perro
 o si quiere salir. Si decide salir, se mostrará todos los perros guardados en 
 el ArrayList.
 
-
+02. Continuando el ejercicio anterior, después de mostrar los perros, al 
+usuario se le pedirá un perro y se recorrerá la lista con un Iterator, se 
+buscará el perro en la lista. Si el perro está en la lista, se eliminará el 
+perro que ingresó el usuario y se mostrará la lista ordenada. Si el perro no 
+se encuentra en la lista, se le informará al usuario y se mostrará la lista 
+ordenada.
 
  */
 
-package guia06_ej_01;
+package guia07_ej_01_02;
 
 /**
  *
