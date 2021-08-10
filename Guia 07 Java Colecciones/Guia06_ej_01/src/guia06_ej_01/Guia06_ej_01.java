949fb56aacb61c34f13813ed5eb783008de885a6
acamus79 <adriancamus1979@gmail.com>
2021-08-09 21:26:43 -0300

    Guia Colecciones Ejercicio 01

---
+++ Guia 07 Java Colecciones/Guia06_ej_01/src/guia06_ej_01/Guia06_ej_01.java
@@ -0,0 +1,22 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package guia06_ej_01;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Guia06_ej_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
