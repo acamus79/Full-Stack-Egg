116bac304bd548485cc90f99fc134ea19130272e
acamus79 <adriancamus1979@gmail.com>
2021-08-10 12:09:02 -0300

    Guia 07 Collections Ejercicio 04

---
+++ Guia 07 Java Colecciones/guia7_ej_04/src/guia7_ej_04/Principal04.java
@@ -0,0 +1,22 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package guia7_ej_04;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Principal04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // El Código va AQUI!!!
+    }
+
+}
