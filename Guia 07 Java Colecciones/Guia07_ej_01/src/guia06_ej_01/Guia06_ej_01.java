c597108ff882922f60982f95be68f2ddc7f9b960
acamus79 <adriancamus1979@gmail.com>
2021-08-09 21:41:59 -0300

    Guia 07 Collections Ejercicio 01

--- Guia 07 Java Colecciones/Guia07_ej_01/src/guia06_ej_01/Guia06_ej_01.java
+++ Guia 07 Java Colecciones/Guia07_ej_01/src/guia06_ej_01/Guia06_ej_01.java
@@ -2,6 +2,15 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Diseñar un programa que lea y guarde razas de perros en un ArrayList de tipo
+String. El programa pedirá una raza de perro en un bucle, el mismo se guardará
+en la lista y después se le preguntará al usuario si quiere guardar otro perro
+o si quiere salir. Si decide salir, se mostrará todos los perros guardados en 
+el ArrayList.
+
+
+
  */
 
 package guia06_ej_01;
