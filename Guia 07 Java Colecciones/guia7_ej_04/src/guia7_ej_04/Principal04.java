d1eef9d8fb8b0ebd4bd5c4fbd6cf70a99ba05578
acamus79 <adriancamus1979@gmail.com>
2021-08-10 12:15:26 -0300

    Guia 07 Collections Ejercicio 04

--- Guia 07 Java Colecciones/guia7_ej_04/src/guia7_ej_04/Principal04.java
+++ Guia 07 Java Colecciones/guia7_ej_04/src/guia7_ej_04/Principal04.java
@@ -2,6 +2,28 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Un cine necesita implementar un sistema en el que se puedan cargar peliculas. 
+Para esto, tendremos una clase Pelicula con el titulo, director y duración de 
+la película (en horas). Implemente las clases y métodos necesarios para esta
+situación, teniendo en cuenta lo que se pide a continuación:
+
+En el main deberemos tener un bucle que crea un objeto Pelicula pidiéndole al
+usuario todos sus datos y guardándolos en el objeto Pelicula.
+Después, esa Pelicula se guarda una lista de Peliculas y se le pregunta al
+usuario si quiere crear otra Pelicula o no.
+
+Después de ese bucle realizaremos las siguientes acciones:
+• Mostrar en pantalla todas las películas.
+• Mostrar en pantalla todas las películas con una duración mayor a 1 hora.
+• Ordenar las películas de acuerdo a su duración (de mayor a menor) y mostrarlo
+en pantalla.
+• Ordenar las películas de acuerdo a su duración (de menor a mayor) y mostrarlo
+en pantalla.
+• Ordenar las películas por titulo, alfabéticamente y mostrarlo en pantalla.
+• Ordenar las películas por director, alfabéticamente y mostrarlo en pantalla.
+Nota: recordar el uso del Comparator para ordenar colecciones con objetos.
+
  */
 
 package guia7_ej_04;
