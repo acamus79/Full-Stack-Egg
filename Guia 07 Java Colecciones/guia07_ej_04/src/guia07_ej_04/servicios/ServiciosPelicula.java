3a26d4f7c8966f10e69dd1bc19ef7d2b42fdfe2a
acamus79 <adriancamus1979@gmail.com>
2021-08-10 21:25:55 -0300

    Guia 07 Collections Ejercicio 04

--- Guia 07 Java Colecciones/guia07_ej_04/src/guia07_ej_04/servicios/ServiciosPelicula.java
+++ Guia 07 Java Colecciones/guia07_ej_04/src/guia07_ej_04/servicios/ServiciosPelicula.java
@@ -2,13 +2,48 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+deberemos tener un bucle que crea un objeto Pelicula pidiéndole al
+usuario todos sus datos y guardándolos en el objeto Pelicula.
+Después, esa Pelicula se guarda una lista de Peliculas y se le pregunta al
+usuario si quiere crear otra Pelicula o no.
+
+Después de ese bucle realizaremos las siguientes acciones:
+• Mostrar en pantalla todas las películas.
+• Mostrar en pantalla todas las películas con una duración mayor a 1 hora.
+• Ordenar las películas de acuerdo a su duración (de mayor a menor) y mostrarlo
+en pantalla.
+• Ordenar las películas de acuerdo a su duración (de menor a mayor) y mostrarlo
+en pantalla.
+• Ordenar las películas por titulo, alfabéticamente y mostrarlo en pantalla.
+• Ordenar las películas por director, alfabéticamente y mostrarlo en pantalla.
+Nota: recordar el uso del Comparator para ordenar colecciones con objetos.
+
  */
 package guia07_ej_04.servicios;
+
+import guia07_ej_04.entidades.Pelicula;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author Adrian E. Camus
  */
 public class ServiciosPelicula {
-    
+
+    private Scanner leer;
+    //aca creo mi Coleccion de Objetos Pelicula
+    private ArrayList<Pelicula> misPelis;
+
+    //constructor vacio pero con instanciacion del scanner y la lista
+    public ServiciosPelicula() {
+        this.leer = new Scanner(System.in).useDelimiter("\n");
+        this.misPelis = new ArrayList();
+    }
+
+    private Pelicula creaPeli() {
+        System.out.println("");
+        return null;
+    }
 }
