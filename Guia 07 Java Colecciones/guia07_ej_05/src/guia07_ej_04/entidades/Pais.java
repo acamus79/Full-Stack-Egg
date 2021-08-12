9255a5f374b0d35f2696e6ebb453be15a7c815da
acamus79 <adriancamus1979@gmail.com>
2021-08-12 13:15:42 -0300

    ej 05

--- Guia 07 Java Colecciones/guia07_ej_05/src/guia07_ej_04/entidades/Pais.java
+++ Guia 07 Java Colecciones/guia07_ej_05/src/guia07_ej_04/entidades/Pais.java
@@ -11,5 +11,29 @@ package guia07_ej_04.entidades;
  * @author Adrian E. Camus
  */
 public class Pais {
+private String nombre;
 
+    public Pais() {
+    }
+
+    public Pais(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPais: " + nombre + "\n";
+    }
+
+
+    
+    
 }
