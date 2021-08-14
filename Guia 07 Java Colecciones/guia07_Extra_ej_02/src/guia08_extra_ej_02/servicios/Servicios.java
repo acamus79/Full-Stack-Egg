e073f735cb7b1ce136b4a18325c40079f72efe0b
acamus79 <adriancamus1979@gmail.com>
2021-08-14 19:12:05 -0300

    Guia07 Java Colecciones Ejercicio Extra 02

---
+++ Guia 07 Java Colecciones/guia07_Extra_ej_02/src/guia08_extra_ej_02/servicios/Servicios.java
@@ -0,0 +1,14 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+package guia08_extra_ej_02.servicios;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Servicios {
+    
+}
