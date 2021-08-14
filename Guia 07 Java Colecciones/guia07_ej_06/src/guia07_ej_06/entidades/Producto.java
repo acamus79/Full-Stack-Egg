5d9145dda7e52a6b156ad4cf2eb7e699eee80138
acamus79 <adriancamus1979@gmail.com>
2021-08-14 14:46:57 -0300

    Guia 07 Java Colecciones Ejercicio 06

---
+++ Guia 07 Java Colecciones/guia07_ej_06/src/guia07_ej_06/entidades/Producto.java
@@ -0,0 +1,15 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package guia07_ej_06.entidades;
+
+/**
+ * 
+ * @author Adrian E. Camus
+ */
+public class Producto {
+
+}
