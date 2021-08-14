3ade9a3a9105c95968c62d541b2f12b39bd4f0a2
acamus79 <adriancamus1979@gmail.com>
2021-08-14 20:49:57 -0300

    Guia07 Java Colecciones Ejercicio Extra 04

---
+++ Guia 07 Java Colecciones/guia07_Extra04/src/guia07_extra04/Guia07_extra04.java
@@ -0,0 +1,41 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+4. Almacena en un HashMap los códigos postales de 10 ciudades a elección de esta
+página: https://mapanet.eu/index.htm. Nota: Poner el código postal sin la letra, solo el
+numero.
+
+• Pedirle al usuario que ingrese 10 códigos postales y sus ciudades.
+
+• Muestra por pantalla los datos introducidos
+
+• Pide un código postal y muestra la ciudad asociada si existe sino avisa al
+usuario.
+
+• Muestra por pantalla los datos
+
+• Agregar una ciudad con su código postal correspondiente más al HashMap.
+
+• Elimina 3 ciudades existentes dentro del HashMap, que pida el usuario.
+
+• Muestra por pantalla los datos
+
+ */
+package guia07_extra04;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Guia07_extra04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
