eecf66205053c8ffb27f2afa5df862aa0ab1c4fa
acamus79 <adriancamus1979@gmail.com>
2021-08-14 19:24:33 -0300

    Guia07 Java Colecciones Ejercicio Extra 02

--- Guia 07 Java Colecciones/guia07_Extra_ej_02/src/guia07_extra_ej_02/entidades/CantanteFamoso.java
+++ Guia 07 Java Colecciones/guia07_Extra_ej_02/src/guia07_extra_ej_02/entidades/CantanteFamoso.java
@@ -10,5 +10,37 @@ package guia07_extra_ej_02.entidades;
  * @author Adrian E. Camus
  */
 public class CantanteFamoso {
+    private String nombre;
+    private String discoConMasVentas;
+
+    public CantanteFamoso() {
+    }
+
+    public CantanteFamoso(String nombre, String discoConMasVentas) {
+        this.nombre = nombre;
+        this.discoConMasVentas = discoConMasVentas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDiscoConMasVentas() {
+        return discoConMasVentas;
+    }
+
+    public void setDiscoConMasVentas(String discoConMasVentas) {
+        this.discoConMasVentas = discoConMasVentas;
+    }
+
+    @Override
+    public String toString() {
+        return "Cantante:       " + nombre + "\n Disco de Mayor Venta:      " + discoConMasVentas;
+    }
+    
     
 }
