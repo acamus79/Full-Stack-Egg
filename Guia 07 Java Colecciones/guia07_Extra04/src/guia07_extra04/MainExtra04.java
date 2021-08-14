2164825df5a90ee4ec718fbfc611ab511f81f795
acamus79 <adriancamus1979@gmail.com>
2021-08-14 20:56:51 -0300

    Guia 07 Java Colecciones Extra 04

---
+++ Guia 07 Java Colecciones/guia07_Extra04/src/guia07_extra04/MainExtra04.java
@@ -0,0 +1,21 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+package guia07_extra04;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class MainExtra04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
