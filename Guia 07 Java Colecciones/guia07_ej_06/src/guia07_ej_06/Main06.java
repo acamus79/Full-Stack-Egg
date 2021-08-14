9fc1f3c65e8bc2313add3a3a6f3e8dbdcc8ae227
acamus79 <adriancamus1979@gmail.com>
2021-08-14 17:10:30 -0300

    Extras

--- Guia 07 Java Colecciones/guia07_ej_06/src/guia07_ej_06/Main06.java
+++ Guia 07 Java Colecciones/guia07_ej_06/src/guia07_ej_06/Main06.java
@@ -2,6 +2,17 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Se necesita una aplicación para una tienda en la cual queremos almacenar los
+distintos productos que venderemos y el precio que tendrán. Además, se necesita
+que la aplicación cuente con las funciones básicas.
+Estas las realizaremos en el main. Como, introducir un elemento, modificar su
+precio, eliminar un producto y mostrar los productos que tenemos con su precio
+(Utilizar Hashmap). El HashMap tendrá de llave el nombre del producto y de valor
+el precio. Realizar un menú para lograr todas las acciones previamente
+mencionadas.
+
+
  */
 package guia07_ej_06;
 
@@ -17,7 +28,6 @@ public class Main06 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         Menu m = new Menu();
         m.menu();
     }
