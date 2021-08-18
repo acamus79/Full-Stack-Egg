db60be4a08309fbc3190c5fbe0ba078ab0008031
acamus79 <adriancamus1979@gmail.com>
2021-08-18 19:23:47 -0300

    Guia 08 Java Relaciones Ejercicio 01

---
+++ Guia 08 Java Relaciones/ej_01/src/ej_01/Ej_01.java
@@ -0,0 +1,21 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+package ej_01;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Ej_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
