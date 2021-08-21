f4db4cda86a816ca856df62a88172de93dc48dd8
acamus79 <adriancamus1979@gmail.com>
2021-08-20 21:30:07 -0300

    Ejercicio 02 Mesa 52

--- Guia 08 Java Relaciones/Ejercicio02_Mesa52/src/servicio/RuletaRusa.java
+++ Guia 08 Java Relaciones/Ejercicio02_Mesa52/src/servicio/RuletaRusa.java
@@ -37,62 +37,57 @@ public class RuletaRusa {
     /**
      * llenarJuego: este método recibe la cantidad de jugadores para guardarlos
      * en la Lista de Jugadores del juego.
+     *
      * @param cantJugadores int entre 1 y 6
      */
     public void llenarJuego() {
         int cantJugadores;
         System.out.println("Cuantos jugadores ?");
         cantJugadores = leer.nextInt();
-        
-        if(cantJugadores>7 || cantJugadores<=0){
-            cantJugadores =6;
+
+        if (cantJugadores > 7 || cantJugadores <= 0) {
+            cantJugadores = 6;
             System.out.println("Solo pueden jugar 6");
         }
-                        
-        for (int i = 0; i < cantJugadores; i++) {
-                Jugador j = new Jugador();
-                System.out.println("Ingrese el nombre del Jugador");
-                j.setNombre(leer.next());
-                j.setId(i+1);
-                listaJugador.add(j);
-            } 
-        }
 
-    
+        for (int i = 0; i < cantJugadores; i++) {
+            Jugador j = new Jugador();
+            System.out.println("Ingrese el nombre del Jugador");
+            j.setNombre(leer.next().toUpperCase());
+            j.setId(i + 1);
+            listaJugador.add(j);
+        }
+    }
+
     /**
      * cada ronda consiste en un jugador que se apunta con el revolver y aprieta
      * el gatillo. Sí el revolver tira el jugador se muere se termina el juego,
      * y los otros se rien sino se muere, se pasa al siguiente jugador hasta que
-     * uno se muera. Si o si alguien se tiene que morir. Al final del juego, 
-     * se debe mostrar que jugador se Murio.
+     * uno se muera. Si o si alguien se tiene que morir. Al final del juego, se
+     * debe mostrar que jugador se Murio.
      */
-    public void ronda(){
+    public void ronda() {
         boolean bandera = false;
         int cont = 1;
         Jugador j = new Jugador();
-        
-        do{
-            
-            
-        for (Jugador aux : listaJugador) {
-            System.out.println("**** RONDA ******");
-            System.out.println("******* "+cont+" *******");
-            cont++;
-            j=aux;
-            colt.siguienteDisparo();
-            if(colt.matar()){
-                j.setMuerto(true);
-                bandera = true;
-                System.out.println(j.toString());
-                break;
-            }       
-        }
-        
-        }while(!bandera);
-        
-        
-        
-        
+
+        do {
+
+            for (Jugador aux : listaJugador) {
+                System.out.println("**** RONDA " + cont + "******");
+                cont++;
+                j = aux;
+                colt.siguienteDisparo();
+                if (colt.matar()) {
+                    j.setMuerto(true);
+                    bandera = true;
+                    System.out.println(j.toString());
+                    break;
+                }
+            }
+
+        } while (!bandera);
+
     }
 
 }
