65c7e6bb32e5d4d8ac75b789030466e401ab2809
acamus79 <adriancamus1979@gmail.com>
2021-08-28 13:57:07 -0300

    Guia09 Java Herencia Ejercicio 01

---
+++ Guia 09 Java Herencia/G09_Ejercicio01/src/entidades/Animal.java
@@ -0,0 +1,72 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+La clase Animal tendrá como atributos el nombre, alimento, edad y raza del
+Animal.
+
+
+ */
+package entidades;
+
+import utilidades.Alimento;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Animal {
+    private String nombre;
+    private String come;
+    private Integer edad;
+    private String raza;
+
+    public Animal(String nombre, String come, Integer edad, String raza) {
+        this.nombre = nombre;
+        this.come = come;
+        this.edad = edad;
+        this.raza = raza;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCome() {
+        return come;
+    }
+
+    public void setCome(String come) {
+        this.come = come;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal de nombre: " + nombre + " y Raza: " + raza 
+                +"tiene " + edad +" años de edad"+" y se alimenta de "+ come;
+    }
+    
+    
+    
+}
