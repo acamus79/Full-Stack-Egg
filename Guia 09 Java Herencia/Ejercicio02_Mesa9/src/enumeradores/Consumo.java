e8382103a5e949697558f317b3837bbd21cda623
acamus79 <adriancamus1979@gmail.com>
2021-08-30 19:31:53 -0300

    Guia09 Java Herencia Ejercicio 02 Mesa 9

---
+++ Guia 09 Java Herencia/Ejercicio02_Mesa9/src/enumeradores/Consumo.java
@@ -0,0 +1,14 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+package enumeradores;
+
+/**
+ *
+ * @author Mesa 9
+ */
+public enum Consumo {
+    A,B,C,D,E,F;
+}
