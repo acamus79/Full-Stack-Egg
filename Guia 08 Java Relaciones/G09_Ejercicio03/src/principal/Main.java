734181848b46cebe3ab29ea6f0bf53fcedb807ca
acamus79 <adriancamus1979@gmail.com>
2021-08-30 14:32:48 -0300

    Guia 09 Java Herencia Ejercicio 03

---
+++ Guia 08 Java Relaciones/G09_Ejercicio03/src/principal/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+3. Siguiendo el ejercicio anterior, en el main vamos a crear un ArrayList de
+Electrodomésticos para guardar 4 electrodomésticos, ya sean lavadoras o
+televisores, con valores ya asignados.
+Luego, recorrer este array y ejecutar el método precioFinal() en cada
+electrodoméstico. Se deberá también mostrar el precio de cada tipo de objeto,
+es decir, el precio de todos los televisores y el de las lavadoras. Una vez hecho
+eso, también deberemos mostrar, la suma del precio de todos los
+Electrodomésticos. Por ejemplo, si tenemos una lavadora con un precio de 2000
+y un televisor de 5000, el resultado final será de 7000 (2000+5000) para
+electrodomésticos, 2000 para lavadora y 5000 para televisor
+
+ */
+package principal;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
