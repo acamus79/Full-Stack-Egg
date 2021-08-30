e5524094bdde2106a7e0401e37c31877a5356aa4
acamus79 <adriancamus1979@gmail.com>
2021-08-30 12:28:10 -0300

    Guia 09 Java Herencia Ejercicio 02

--- Guia 09 Java Herencia/G09_Ejercicio02/src/entidades/Televisor.java
+++ Guia 09 Java Herencia/G09_Ejercicio02/src/entidades/Televisor.java
@@ -2,6 +2,28 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Se debe crear también una subclase llamada Televisor con los siguientes
+atributos: resolución (en pulgadas) y sintonizador TDT (booleano), además de los
+atributos heredados.
+Los constructores que se implementarán serán:
+• Un constructor vacío.
+• Un constructor con la resolución, sintonizador TDT y el resto de atributos
+heredados. Recuerda que debes llamar al constructor de la clase padre.
+Los métodos que se implementara serán:
+• Método get y set de los atributos resolución y sintonizador TDT.
+• Método crearTelevisor(): este método llama a crearElectrodomestico() de la
+clase padre, lo utilizamos para llenar los atributos heredados del padre y
+después llenamos los atributos del televisor.
+• Método precioFinal(): este método será heredado y se le sumará la siguiente
+funcionalidad. Si el televisor tiene una resolución mayor de 40 pulgadas, se
+incrementará el precio un 30% y si tiene un sintonizador TDT incorporado,
+aumentará $500. Recuerda que las condiciones que hemos visto en la clase
+Electrodomestico también deben afectar al precio.
+Finalmente, en el main debemos realizar lo siguiente:
+Vamos a crear una Lavadora y un Televisor y llamar a los métodos necesarios
+para mostrar el precio final de los dos electrodomésticos
+
  */
 package entidades;
 
@@ -10,5 +32,10 @@ package entidades;
  * @author Adrian E. Camus
  */
 public class Televisor extends Electrodomestico {
+    
+    private int resolucionPulg;
+    private boolean sintTDT;
+    
+    
     
 }
