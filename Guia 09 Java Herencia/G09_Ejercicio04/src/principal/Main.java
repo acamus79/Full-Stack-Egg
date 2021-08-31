56464d4d2abe636b9329d87e0fc569ae8ed06730
acamus79 <adriancamus1979@gmail.com>
2021-08-30 22:27:01 -0300

    Guia09 Java Herencia Ejercicio 04

--- Guia 09 Java Herencia/G09_Ejercicio04/src/principal/Main.java
+++ Guia 09 Java Herencia/G09_Ejercicio04/src/principal/Main.java
@@ -8,7 +8,6 @@ de formas geométricas, en este caso un circulo y un rectángulo. Ya que este
 calculo se va a repetir en las dos formas geométricas, vamos a crear una Interfaz,
 llamada calculosFormas que tendrá, los dos métodos para calcular el área, el
 perímetro y el valor de PI como constante.
-13
 Desarrollar el ejercicio para que las formas implementen los métodos de la
 interfaz y se calcule el área y el perímetro de los dos. En el main se crearán las
 formas y se mostrará el resultado final.
