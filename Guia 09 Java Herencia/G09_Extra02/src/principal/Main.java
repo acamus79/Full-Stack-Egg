541eda96c2ebfd6418ad0993675eb46e748148c4
acamus79 <adriancamus1979@gmail.com>
2021-08-31 22:53:01 -0300

    Guia09 Java Herencia Ejercicio Extra 02

---
+++ Guia 09 Java Herencia/G09_Extra02/src/principal/Main.java
@@ -0,0 +1,48 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+Crear una superclase llamada Edificio con los siguientes atributos: ancho, alto y
+largo. La clase edificio tendrá como métodos:
+• Método calcularSuperficie(): calcula la superficie del edificio.
+• Método calcularVolumen(): calcula el volumen del edifico.
+Estos métodos serán abstractos y los implementarán las siguientes clases:
+• Clase Polideportivo con su nombre y tipo de instalación que puede ser
+Techado o Abierto, esta clase implementará los dos métodos abstractos y los
+atributos del padre.
+15
+• Clase EdificioDeOficinas con sus atributos número de oficinas, cantidad de
+personas por oficina y numero de pisos. Esta clase implementará los dos
+métodos abstractos y los atributos del padre.
+De esta clase nos interesa saber cuantas personas pueden trabajar en todo el
+edificio, el usuario dirá cuantas personas entran en cada oficina, cuantas
+oficinas y el numero de piso (suponiendo que en cada piso hay una oficina).
+Crear el método cantPersonas(), que muestre cuantas personas entrarían en
+un piso y cuantas en todo el edificio.
+Por ultimo, en el main vamos a crear un ArrayList de tipo Edificio. El ArrayList debe
+contener dos polideportivos y dos edificios de oficinas. Luego, recorrer este array
+y ejecutar los métodos calcularSuperficie y calcularVolumen en cada Edificio. Se
+deberá mostrar la superficie y el volumen de cada edificio.
+Además de esto, para la clase Polideportivo nos interesa saber cuantos
+polideportivos son techados y cuantos abiertos. Y para la clase EdificioDeOficinas
+deberemos llamar al método cantPersonas() y mostrar los resultados de cada
+edificio de oficinas
+
+ */
+package principal;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
