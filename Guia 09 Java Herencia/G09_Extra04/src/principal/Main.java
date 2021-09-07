85c074757d1a07ef9f4a8f813112a47340efa81c
acamus79 <adriancamus1979@gmail.com>
2021-09-06 23:57:55 -0300

    Guia 09 Java Herencia Extra 04

---
+++ Guia 09 Java Herencia/G09_Extra04/src/principal/Main.java
@@ -0,0 +1,49 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+4. Sistema Gestión Facultad. Se pretende realizar una aplicación para una facultad
+que gestione la información sobre las personas vinculadas con la misma y que se
+pueden clasificar en tres tipos: estudiantes, profesores y personal de servicio. A
+continuación, se detalla qué tipo de información debe gestionar esta aplicación:
+• Por cada persona, se debe conocer, al menos, su nombre y apellidos, su número
+de identificación y su estado civil.
+• Con respecto a los empleados, sean del tipo que sean, hay que saber su año de
+incorporación a la facultad y qué número de despacho tienen asignado.
+• En cuanto a los estudiantes, se requiere almacenar el curso en el que están
+matriculados.
+• Por lo que se refiere a los profesores, es necesario gestionar a qué departamento
+pertenecen (lenguajes, matemáticas, arquitectura, ...).
+• Sobre el personal de servicio, hay que conocer a qué sección están asignados
+(biblioteca, decanato, secretaría, ...).
+El ejercicio consiste, en primer lugar, en definir la jerarquía de clases de esta
+aplicación. A continuación, debe programar las clases definidas en las que,
+además de los constructores, hay que desarrollar los métodos correspondientes
+a las siguientes operaciones:
+• Cambio del estado civil de una persona.
+• Reasignación de despacho a un empleado.
+• Matriculación de un estudiante en un nuevo curso.
+• Cambio de departamento de un profesor.
+• Traslado de sección de un empleado del personal de servicio.
+• Imprimir toda la información de cada tipo de individuo. Incluya un programa
+de prueba que instancie objetos de los distintos tipos y pruebe los métodos
+desarrollados.
+
+ */
+package principal;
+
+/**
+ *
+ * @author Adrian E. Camus
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
