f61087740fee60592b5ccd29e163f924f8eba79b
acamus79 <adriancamus1979@gmail.com>
2021-09-23 19:59:48 -0300

    Update ServicioFabricante.java

--- Guia 12 JDBC/Tienda/src/servicio/ServicioFabricante.java
+++ Guia 12 JDBC/Tienda/src/servicio/ServicioFabricante.java
@@ -5,11 +5,122 @@
  */
 package servicio;
 
+import entidad.Fabricante;
+import excepcion.MiExcepcion;
+import java.util.List;
+import persistencia.FabricanteDAO;
+
 /**
  *
  * @author Adrian E. Camus
  */
 public class ServicioFabricante {
+    
+    private FabricanteDAO f;
+
+    public ServicioFabricante() {
+    f = new FabricanteDAO();
+    }
+    
+    public void crearFabricante(Integer codigo, String nombre) throws MiExcepcion {
+        try {
+            // VALIDACIÓN
+            if (codigo == null | codigo < 0) {
+                throw new MiExcepcion("DEBE INGRESAR UN CODIGO");
+            }
+
+            if (nombre == null | nombre.trim().isEmpty()) {
+                throw new MiExcepcion("DEBE INGRESAR UN NOMBRE");
+            }
+
+            Fabricante fabricante = new Fabricante();
+
+            fabricante.setCodigo(codigo);
+            fabricante.setNombre(nombre);
+            f.guardarFabricante(fabricante);
+            
+        } catch (MiExcepcion e) {
+            // e.printStackTrace();
+            throw e;
+        } catch (Exception e) {
+            // e.printStackTrace();
+            throw new MiExcepcion("ERROR DE SISTEMA");
+        }
+    }
+
+    public void modificarFabricante(Integer codigo, String nombre) throws MiExcepcion {
+        try {
+            // VALIDACIÓN
+            if (codigo == null | codigo < 0) {
+                throw new MiExcepcion("DEBE INGRESAR UN CODIGO");
+            }
+
+            if (nombre == null | nombre.trim().isEmpty()) {
+                throw new MiExcepcion("DEBE INGRESAR UN NOMBRE");
+            }
+
+            Fabricante fabricante = f.buscarFabricantePorCodigo(codigo);
+
+            if (fabricante == null) {
+                throw new MiExcepcion("EL CODIGO INGRESADO NO ESTÁ ASOCIADO A NINGÚN FABRICANTE");
+            }
+
+            fabricante.setNombre(nombre);
+
+            f.modificarFabricante(fabricante);
+        } catch (MiExcepcion e) {
+            // e.printStackTrace();
+            throw e;
+        } catch (Exception e) {
+            // e.printStackTrace();
+            throw new MiExcepcion("ERROR DE SISTEMA");
+        }
+    }
+
+    public void eliminarUsuario(String correo) throws MiExcepcion {
+        try {
+            if (correo == null || correo.trim().isEmpty()) {
+                throw new MiExcepcion("DEBE INGRESAR UN CORREO");
+            }
+
+            Usuario fabricante = fabricanteDAO.buscarUsuarioPorCorreo(correo);
+
+            if (fabricante == null) {
+                throw new MiExcepcion("EL CORREO INGRESADO NO ESTÁ ASOCIADO A NINGÚN USUARIO");
+            }
+
+            fabricanteDAO.eliminarUsuario(correo);
+        } catch (MiExcepcion e) {
+            // e.printStackTrace();
+            throw e;
+        } catch (Exception e) {
+            // e.printStackTrace();
+            throw new MiExcepcion("ERROR DE SISTEMA");
+        }
+    }
+
+    public void imprimirUsuarios() throws MiExcepcion {
+        try {
+            List<Usuario> fabricantes = fabricanteDAO.obtenerUsuarios();
+
+            if (fabricantes.isEmpty()) {
+                throw new MiExcepcion("NO EXISTEN USUARIOS");
+            } else {
+                System.out.println("*** LISTA DE USUARIOS ***");
+                System.out.printf("%-20s%-15s%-15s\n", "CORREO", "NOMBRE", "APELLIDO"); // FORMATO DE IMPRESIÓN
+                for (Usuario fabricante : fabricantes) {
+                    System.out.println(fabricante);
+                }
+                System.out.println();
+            }
+        } catch (MiExcepcion e) {
+            // e.printStackTrace();
+            throw e;
+        } catch (Exception e) {
+            // e.printStackTrace();
+            throw new MiExcepcion("ERROR DE SISTEMA");
+        }
+    }
     
     
     
