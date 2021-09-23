d4fa5489e4f8af4b41c690ea9899cf1b56d9cedf
acamus79 <adriancamus1979@gmail.com>
2021-09-23 19:59:43 -0300

    Update FabricanteDAO.java

--- Guia 12 JDBC/Tienda/src/persistencia/FabricanteDAO.java
+++ Guia 12 JDBC/Tienda/src/persistencia/FabricanteDAO.java
@@ -7,35 +7,72 @@ package persistencia;
 
 import entidad.Fabricante;
 import excepcion.MiExcepcion;
+import java.sql.SQLException;
 
 /**
  *
  * @author Adrian E. Camus
  */
-public class FabricanteDAO extends DAO{
-    
-    
-    
-    
-    
-    public void guardarFabricante(Fabricante f) throws MiExcepcion{
+public class FabricanteDAO extends DAO {
+
+    public void guardarFabricante(Fabricante f) throws MiExcepcion {
         try {
-            if (f == null){
+            if (f == null) {
                 throw new MiExcepcion("FABRICANTE INVÁLIDO");
             }
             // SENTENCIA SQL DE INSERCIÓN -- INSERT INTO `tienda`.`fabricante` (`codigo`, `nombre`) VALUES ('10', 'pHILLIPS');
             String sql = "INSERT INTO fabricante (codigo, nombre) "
                     + "VALUES('" + f.getCodigo()
-                    + "', '" + f.getNombre()+ "');";
-            insertarModificarEliminar(sql);     
-                        
-        }catch(MiExcepcion e) {
+                    + "', '" + f.getNombre() + "');";
+            insertarModificarEliminar(sql);
+
+        } catch (MiExcepcion e) {
             System.out.println(e.getMessage());
             throw new MiExcepcion("ERROR AL GUARDAR USUARIO");
         }
-        
+
     }
-    
-    
-    
+
+    public Fabricante buscarFabricantePorCodigo(int cod) throws MiExcepcion {
+
+        try {
+            // SENTENCIA SQL DE CONSULTA
+            String sql = "SELECT * FROM fabricante WHERE codigo = '" + cod + "';";
+            consultarBase(sql);
+            Fabricante f = null;
+
+            while (resultado.next()) {
+                f = new Fabricante();
+
+                f.setCodigo(resultado.getInt(1));
+                f.setNombre(resultado.getString(2));
+            }
+            return f;
+        } catch (SQLException | MiExcepcion e) {
+            System.out.println(e.getMessage());
+            throw new MiExcepcion("ERROR AL OBTENER USUARIO");
+        } finally {
+            desconectarBase();
+        }
+
+    }
+
+    public void modificarFabricante(Fabricante f) throws MiExcepcion {
+
+        try {
+            if (f == null) {
+                throw new MiExcepcion("FABRICANTE INVÁLIDO");
+            }
+            // SENTENCIA SQL DE MODIFICACIÓN
+            String sql = "UPDATE fabricante SET nombre = '" + f.getNombre() + "', "
+                    + "WHERE codigo = '" + f.getCodigo() + "';";
+
+            insertarModificarEliminar(sql);
+        } catch (MiExcepcion e) {
+            System.out.println(e.getMessage());
+            throw new MiExcepcion("ERROR AL MODIFICAR FABRICANTE");
+        }
+
+    }
+
 }
