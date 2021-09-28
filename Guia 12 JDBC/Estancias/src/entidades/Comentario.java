658ae0251b2a3b6adc74ad54ab19f76125e48a21
acamus79 <adriancamus1979@gmail.com>
2021-09-28 13:18:37 -0300

    Update Comentario.java

--- Guia 12 JDBC/Estancias/src/entidades/Comentario.java
+++ Guia 12 JDBC/Estancias/src/entidades/Comentario.java
@@ -11,5 +11,42 @@ package entidades;
  * @author Adrian E. Camus
  */
 public class Comentario {
+    private int id_comentario;
+    private Casa casa;
+    private String comentario;
+
+    public Comentario() {
+    }
+
+    public Comentario(int id_comentario, Casa casa, String comentario) {
+        this.id_comentario = id_comentario;
+        this.casa = casa;
+        this.comentario = comentario;
+    }
+
+    public int getId_comentario() {
+        return id_comentario;
+    }
+
+    public void setId_comentario(int id_comentario) {
+        this.id_comentario = id_comentario;
+    }
+
+    public Casa getCasa() {
+        return casa;
+    }
+
+    public void setCasa(Casa casa) {
+        this.casa = casa;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
 
 }
