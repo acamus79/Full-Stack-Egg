a5b1d1b2c36c03760c00fc191455aa97a8d897ab
acamus79 <adriancamus1979@gmail.com>
2021-09-28 11:14:47 -0300

    Update FamiliaDAO.java

--- Guia 12 JDBC/Estancias/src/persistencia/FamiliaDAO.java
+++ Guia 12 JDBC/Estancias/src/persistencia/FamiliaDAO.java
@@ -92,14 +92,13 @@ public class FamiliaDAO extends DAO{
         }
 
     }
-/*
     
-    public boolean buscarProductoPorFab(int cod_fab) throws MiExcepcion {
+    public boolean buscarFamiliaPorCasa(int id_casa) throws MiExcepcion {
 
         try
         {
             // SENTENCIA SQL DE CONSULTA
-            String sql = "SELECT * FROM fabricante WHERE codigo = '" + cod_fab + "';";
+            String sql = "SELECT * FROM familias WHERE id_casa_familia = '" + id_casa + "';";
             consultarBase(sql);
             return resultado.next();
             
@@ -115,19 +114,22 @@ public class FamiliaDAO extends DAO{
     }
 
     //MODIFICAR
-    public void modificarProducto(Producto p) throws MiExcepcion {
+    public void modificarFamilia(Familia f) throws MiExcepcion {
 
         try
         {
-            if (p == null)
+            if (f == null)
             {
-                throw new MiExcepcion("PRODUCTO INVÁLIDO");
+                throw new MiExcepcion("FAMILIA INVALIDA");
             }
             // SENTENCIA SQL DE MODIFICACIÓN
-            String sql = "UPDATE producto SET nombre = '" + p.getNombre() + "', "
-                    + "apellido = '" + p.getPrecio() + "' "
-                    + "apellido = '" + p.getCodigoFabricante() + "' "
-                    + "WHERE codigo = '" + p.getCodigo() + "';";
+            String sql = "UPDATE familias SET nombre = '" + f.getNombre() + "', "
+                    + "edad_minima = '" + f.getEdad_minima() + "' "
+                    + "edad_maxima = '" + f.getEdad_maxima() + "' "
+                    + "num_hijos = '" + f.getNum_hijos() + "' "
+                    + "email = '" + f.getEmail() + "' "
+                    + "id_casa_familia = '" + f.getEmail() + "' "
+                    + "WHERE id_familia = '" + f.getId() + "';";
 
             insertarModificarEliminar(sql);
         } catch (MiExcepcion e)
@@ -139,44 +141,65 @@ public class FamiliaDAO extends DAO{
     }
 
     //ELIMINAR
-    public void eliminarProducto(Integer cod) throws MiExcepcion {
+    public void eliminarFamilia(Integer id) throws MiExcepcion {
         try
         {
             // SENTENCIA SQL DE ELIMINACIÓN
-            String sql = "DELETE FROM producto WHERE codigo = '" + cod + "';";
+            String sql = "DELETE FROM familias WHERE id_familia = '" + id + "';";
 
             insertarModificarEliminar(sql);
         } catch (MiExcepcion e)
         {
             System.out.println(e.getMessage());
-            throw new MiExcepcion("ERROR AL ELIMINAR PRODUCTO");
+            throw new MiExcepcion("ERROR AL ELIMINAR FAMILIA");
         }
 
     }
 
     //OBTENER
-    public List<Producto> obtenerProducto() throws MiExcepcion {
+    public List<Familia> obtenerFamilia() throws MiExcepcion {
         try
         {
-            // SENTENCIA SQL DE CONSULTA
-            String sql = "SELECT * FROM producto";
+            // SENTENCIA SQL DE CONSULTA CON INNER JOIN
+            String sql = "SELECT * FROM familias f"
+                    + " INNER JOIN casas c on c.id_casa = f.id_casa_familia;";
+                    
             consultarBase(sql);
 
-            List<Producto> productos = new ArrayList<>();
-            Producto aux = null;
+            List<Familia> familias = new ArrayList<>();
+            Familia f = null;
+            Casa c = null;
 
             while (resultado.next())
             {
-                aux = new Producto();
-                aux.setCodigo(resultado.getInt(1));
-                aux.setNombre(resultado.getString(2));
-                aux.setPrecio(resultado.getDouble(3));
-                aux.setCodigoFabricante(resultado.getInt(4));
-
-                productos.add(aux);
+                f = new Familia();
+                c= new Casa();
+                
+                f.setId(resultado.getInt(1));
+                f.setNombre(resultado.getString(2));
+                f.setEdad_minima(resultado.getInt(3));
+                f.setEdad_maxima(resultado.getInt(4));
+                f.setNum_hijos(resultado.getInt(5));
+                f.setEmail(resultado.getString(6));
+                f.setId_casa_familia(resultado.getInt(7));
+                
+                //Ahora seteo la Casa
+                c.setId_casa(resultado.getInt(8));
+                c.setCalle(resultado.getNString(9));
+                c.setNumero(resultado.getInt(10));
+                c.setCodigo_postal(resultado.getString(11));
+                c.setCiudad(resultado.getString(12));
+                c.setPais(resultado.getString(13));
+                c.setFecha_desde(resultado.getDate(14));
+                c.setFecha_hasta(resultado.getDate(15));
+                c.setTiempo_minimo(resultado.getInt(16));
+                c.setTiempo_maximo(resultado.getInt(17));
+                // y recien ahora seteo el objeto casa a la familia
+                f.setCasa(c);
+                //y ahora agrego el objeto familia a la lista
+                familias.add(f);
             }
-
-            return productos;
+            return familias;
         } catch (SQLException | MiExcepcion e)
         {
             System.out.println(e.getMessage());
@@ -187,6 +210,6 @@ public class FamiliaDAO extends DAO{
         }
     }
 
-  */  
+  
     
 }
