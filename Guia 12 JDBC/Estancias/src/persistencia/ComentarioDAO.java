3a45ee82ef79863679e758eaf5cd7764558f7b12
acamus79 <adriancamus1979@gmail.com>
2021-09-28 15:21:44 -0300

    Update ComentarioDAO.java

--- Guia 12 JDBC/Estancias/src/persistencia/ComentarioDAO.java
+++ Guia 12 JDBC/Estancias/src/persistencia/ComentarioDAO.java
@@ -113,96 +113,89 @@ public class ComentarioDAO extends DAO{
     }
 
     //MODIFICAR
-    public void modificarFamilia(Familia f) throws MiExcepcion {
+    public void modificarComentario(Comentario c) throws MiExcepcion {
 
         try
         {
-            if (f == null)
+            if (c == null)
             {
-                throw new MiExcepcion("FAMILIA INVALIDA");
+                throw new MiExcepcion("COMENTARIO INVALIDA");
             }
             // SENTENCIA SQL DE MODIFICACIÓN
-            String sql = "UPDATE familias SET nombre = '" + f.getNombre() + "', "
-                    + "edad_minima = '" + f.getEdad_minima() + "' "
-                    + "edad_maxima = '" + f.getEdad_maxima() + "' "
-                    + "num_hijos = '" + f.getNum_hijos() + "' "
-                    + "email = '" + f.getEmail() + "' "
-                    + "id_casa_familia = '" + f.getEmail() + "' "
-                    + "WHERE id_familia = '" + f.getId() + "';";
+            String sql = "UPDATE comentarios SET id_casa = '" + c.getCasa().getId_casa() + "', "
+                    + "comentario = '" + c.getComentario() + "' "
+                    + "WHERE id_comentario = '" + c.getId_comentario() + "';";
 
             insertarModificarEliminar(sql);
         } catch (MiExcepcion e)
         {
             System.out.println(e.getMessage());
-            throw new MiExcepcion("ERROR AL MODIFICAR PRODUCTO");
+            throw new MiExcepcion("ERROR AL MODIFICAR EL COMENTARIO");
         }
 
     }
 
     //ELIMINAR
-    public void eliminarFamilia(Integer id) throws MiExcepcion {
+    public void eliminarComentario(Integer id) throws MiExcepcion {
         try
         {
             // SENTENCIA SQL DE ELIMINACIÓN
-            String sql = "DELETE FROM familias WHERE id_familia = '" + id + "';";
+            String sql = "DELETE FROM comentarios WHERE id_comentario = '" + id + "';";
 
             insertarModificarEliminar(sql);
         } catch (MiExcepcion e)
         {
             System.out.println(e.getMessage());
-            throw new MiExcepcion("ERROR AL ELIMINAR FAMILIA");
+            throw new MiExcepcion("ERROR AL ELIMINAR COMENTARIO");
         }
 
     }
 
     //OBTENER
-    public List<Familia> obtenerFamilia() throws MiExcepcion {
+    public List<Comentario> obtenerComentario() throws MiExcepcion {
         try
         {
             // SENTENCIA SQL DE CONSULTA CON INNER JOIN
-            String sql = "SELECT * FROM familias f"
-                    + " INNER JOIN casas c on c.id_casa = f.id_casa_familia;";
+            String sql = "SELECT * FROM comentarios com"
+                    + " INNER JOIN casas c on c.id_casa = com.id_casa_familia;";
 
             consultarBase(sql);
 
-            List<Familia> familias = new ArrayList<>();
-            Familia f = null;
+            List<Comentario> comentarios = new ArrayList<>();
+            Comentario aux = null;
             Casa c = null;
 
             while (resultado.next())
             {
-                f = new Familia();
+                aux = new Comentario();
                 c = new Casa();
 
-                f.setId(resultado.getInt(1));
-                f.setNombre(resultado.getString(2));
-                f.setEdad_minima(resultado.getInt(3));
-                f.setEdad_maxima(resultado.getInt(4));
-                f.setNum_hijos(resultado.getInt(5));
-                f.setEmail(resultado.getString(6));
-                f.setId_casa_familia(resultado.getInt(7));
-
-                //Ahora seteo la Casa
-                c.setId_casa(resultado.getInt(8));
-                c.setCalle(resultado.getNString(9));
-                c.setNumero(resultado.getInt(10));
-                c.setCodigo_postal(resultado.getString(11));
-                c.setCiudad(resultado.getString(12));
-                c.setPais(resultado.getString(13));
-                c.setFecha_desde(resultado.getDate(14));
-                c.setFecha_hasta(resultado.getDate(15));
-                c.setTiempo_minimo(resultado.getInt(16));
-                c.setTiempo_maximo(resultado.getInt(17));
-                // y recien ahora seteo el objeto casa a la familia
-                f.setCasa(c);
-                //y ahora agrego el objeto familia a la lista
-                familias.add(f);
+                aux.setId_comentario(resultado.getInt(1));
+                aux.setComentario(resultado.getString(3));
+                                
+                //Ahora la Casa
+                c.setId_casa(resultado.getInt(4));
+                c.setCalle(resultado.getNString(5));
+                c.setNumero(resultado.getInt(6));
+                c.setCodigo_postal(resultado.getString(7));
+                c.setCiudad(resultado.getString(8));
+                c.setPais(resultado.getString(9));
+                c.setFecha_desde(resultado.getDate(10));
+                c.setFecha_hasta(resultado.getDate(11));
+                c.setTiempo_minimo(resultado.getInt(12));
+                c.setTiempo_maximo(resultado.getInt(13));
+                c.setPrecio_habitacion(resultado.getDouble(14));
+                c.setTipo_vivienda(resultado.getString(15));
+                // y recien ahora seteo el objeto casa al comentario
+                aux.setCasa(c);
+                //y ahora agrego el objeto aux a la lista de comentarios
+                comentarios.add(aux);
             }
-            return familias;
+            return comentarios;
         } catch (SQLException | MiExcepcion e)
         {
             System.out.println(e.getMessage());
-            throw new MiExcepcion("ERROR AL OBTENER PRODUCTOS");
+            throw new MiExcepcion("ERROR AL OBTENER COMENTARIOS");
         } finally
         {
             desconectarBase();
