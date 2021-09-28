c5fedbe82a043454cd845acc30dc49800319beb9
acamus79 <adriancamus1979@gmail.com>
2021-09-28 12:17:31 -0300

    Create Cliente.java

---
+++ Guia 12 JDBC/Estancias/src/entidades/Cliente.java
@@ -0,0 +1,92 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package entidades;
+
+/**
+ * 
+ * @author Adrian E. Camus
+ */
+public class Cliente {
+
+  private int id_cliente;
+  private String nombre;
+  private String calle;
+  private int numero;
+  private String codigo_postal;
+  private String ciudad;
+  private String pais;
+  private String email;
+
+    public Cliente() {
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCodigo_postal() {
+        return codigo_postal;
+    }
+
+    public void setCodigo_postal(String codigo_postal) {
+        this.codigo_postal = codigo_postal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+  
+      
+}
