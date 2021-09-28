c62318d6937073f3a9eb2c14c20e8e56bac860c8
acamus79 <adriancamus1979@gmail.com>
2021-09-28 15:37:38 -0300

    Create EstanciaDAO.java

---
+++ Guia 12 JDBC/Estancias/src/persistencia/EstanciaDAO.java
@@ -0,0 +1,18 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package persistencia;
+
+/**
+ * 
+ * @author Adrian E. Camus
+ */
+public class EstanciaDAO extends DAO {
+
+    
+    
+    
+}
