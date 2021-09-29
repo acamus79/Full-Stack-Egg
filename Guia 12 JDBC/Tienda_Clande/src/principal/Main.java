1c353efd24114bd4d25116d2d6c890c3987eaf25
acamus79 <adriancamus1979@gmail.com>
2021-09-28 21:25:29 -0300

    Update Main.java

--- Guia 12 JDBC/Tienda_Clande/src/principal/Main.java
+++ Guia 12 JDBC/Tienda_Clande/src/principal/Main.java
@@ -52,10 +52,15 @@ y algunos servicios para manejar operaciones muy específicas como las estadíst
 
 Realizar un menú en java a través del cual se permita elegir qué consulta se desea
 realizar. Las consultas a realizar sobre la BD son las siguientes:
+
 a) Lista el nombre de todos los productos que hay en la tabla producto. 
+
 b) Lista los nombres y los precios de todos los productos de la tabla producto. 
+
 c) Listar aquellos productos que su precio esté entre 120 y 202. 
+
 d) Buscar y listar todos los Portátiles de la tabla producto. 
+
 e) Listar el nombre y el precio del producto más barato. 
 f) Ingresar un producto a la base de datos.
 g) Ingresar un fabricante a la base de datos
