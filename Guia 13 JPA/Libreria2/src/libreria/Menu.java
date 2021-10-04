afc10acbc691a976065613f055f20e299bea9be4
acamus79 <adriancamus1979@gmail.com>
2021-10-04 14:14:12 -0300

    Update Menu.java

--- Guia 13 JPA/Libreria2/src/libreria/Menu.java
+++ Guia 13 JPA/Libreria2/src/libreria/Menu.java
@@ -2,6 +2,13 @@
  * Sin licencia.
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
+
+8) Búsqueda de un Autor por nombre.
+9) Búsqueda de un libro por ISBN.
+10) Búsqueda de un libro por Título.
+11) Búsqueda de un libro/s por nombre de Autor.
+12) Búsqueda de un libro/s por nombre de Editorial.
+
  */
 package libreria;
 
@@ -34,25 +41,22 @@ public class Menu {
         {
             try
             {
-                System.out.println("***** BIBLIOTECA ******");
-                System.out.println("** 1. MOSTRAR LIBROS **");
-                System.out.println("** 2. PRESTAMO       **");
-                System.out.println("** 3. DEVOLUCION     **");
-                System.out.println("** 4. VENTA          **");//este va de yapa
-                System.out.println("** 5. AGREGAR LIBRO  **");
-                System.out.println("** 6. BORRAR LIBRO   **");
-                System.out.println("** 7. PARA SALIR     **");
-                System.out.println("***********************");
+                System.out.println("*****      BIBLIOTECA     ******");
+                System.out.println("** 1. ADMINISTRAR LIBROS      **");
+                System.out.println("** 2. ADMINISTRAR AUTORES     **");
+                System.out.println("** 3. ADMINISTRAR EDITORIALES **");
+                System.out.println("** 4. PARA SALIR              **");
+                System.out.println("********************************");
                 System.out.println("Ingrese la opcion...\n");
                 op = sc.nextInt();
 
                 switch (op)
                 {
                     case 1:
-
+                        menuLibro();
                         break;
                     case 2:
-
+                        menuAutor();
                         break;
                     case 3:
 
@@ -83,7 +87,118 @@ public class Menu {
         } while (op != 7);
     }
 
+    private void menuLibro() {
+        int op = 0;
+
+        do
+        {
+            try
+            {
+                System.out.println("*****   LIBROS    ******");
+                System.out.println("** 1. MOSTRAR LIBROS **");
+                System.out.println("** 2. PRESTAMO       **");
+                System.out.println("** 3. DEVOLUCION     **");
+                System.out.println("** 4. VENTA          **");
+                System.out.println("** 5. AGREGAR LIBRO  **");
+                System.out.println("** 6. BORRAR LIBRO   **");
+                System.out.println("** 7. BUSCAR LIBRO   **");
+                System.out.println("** 8. VOLVER         **");
+                System.out.println("***********************");
+                System.out.println("Ingrese la opcion...\n");
+                op = sc.nextInt();
+
+                switch (op)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+                        System.out.println("OPCION EN DESARROLLO");
+                        break;
+                    case 3:
+                        System.out.println("OPCION EN DESARROLLO");
+                        break;
+                    case 4:
+                        System.out.println("OPCION EN DESARROLLO");
+                        break;
+                    case 5:
+                        
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    case 8:
+                        System.out.println("\n- VOLVIENDO AL MENU PRINCIPAL -\n");
+                        break;
+                    default:
+                        System.out.println("Ingresó una opcion no validad");
+                        op = 0;
+                }
+
+            } catch (InputMismatchException e)
+            {
+                e.getMessage();
+                System.out.println("PAPARULO NO METAS LETRAS\n\n");
+                sc.next();
+            }
+        } while (op != 8);
+    }
+
+    private void menuAutor() {
+        int op = 0;
+
+        do
+        {
+            try
+            {
+                System.out.println("*****  AUTORES    ******");
+                System.out.println("** 1. MOSTRAR AUTORES **");
+                System.out.println("** 2. AGREGAR AUTOR   **");
+                System.out.println("** 3. BORRAR AUTOR    **");
+                System.out.println("** 4. BUSCAR AUTOR    **");
+                System.out.println("** 5. VOLVER          **");
+                System.out.println("************************");
+                System.out.println("Ingrese la opcion...\n");
+                op = sc.nextInt();
+
+                switch (op)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        
+                        break;
+                    case 4:
+                        
+                        break;
+                    case 5:
+                        System.out.println("\n- VOLVIENDO AL MENU PRINCIPAL -\n");
+                        break;
+                    default:
+                        System.out.println("Ingresó una opcion no validad");
+                        op = 0;
+                }
+
+            } catch (InputMismatchException e)
+            {
+                e.getMessage();
+                System.out.println("PAPARULO NO METAS LETRAS\n\n");
+                sc.next();
+            }
+        } while (op != 5);
+    }
+
     
     
     
+    /*
+    
+     */
 }
