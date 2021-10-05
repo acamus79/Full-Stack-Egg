52f7b642bda56e41e04c75373823f79f78f31ef9
acamus79 <adriancamus1979@gmail.com>
2021-10-05 18:38:49 -0300

    Update Libreria.java

--- Guia 13 JPA/Libreria/src/libreria/Libreria.java
+++ Guia 13 JPA/Libreria/src/libreria/Libreria.java
@@ -26,7 +26,7 @@ public class Libreria {
         EntityManager em = emf.createEntityManager();
         
         ServicioAutor au = new ServicioAutor();
-        ServicioEditorial editorSv = new ServicioEditorial();
+        ServicioEditorial eu = new ServicioEditorial();
         ServicioLibro lu = new ServicioLibro();
 //        
 //        Autor au1 = au.creaautor("Cortazar");
@@ -40,50 +40,48 @@ public class Libreria {
 //        
 //        m.menu();
         
-        Autor aut1 = au.creaautor("Cortazar");
-        Autor aut2 = au.creaautor("Cortazar");
-        Autor aut3 = au.creaautor("Cortazar");
-        Autor aut4 = au.creaautor("Cortazar");
-        Autor aut5 = au.creaautor("Cortazar");
+        
         
 
 
-        Editorial edit1 = editorSv.creaEditorial("MINOTAURO");
-        Editorial edit2 =editorSv.creaEditorial("SUDAMERICANA");
-        Editorial edit3 =editorSv.creaEditorial("PENGUIN RANDOM HOUSE");
-        Editorial edit4 =editorSv.creaEditorial("PLANETA");
-        Editorial edit5 =editorSv.creaEditorial("EMECE");
-        Editorial edit6 =editorSv.creaEditorial("SANTILLANA");
-        Editorial edit7 =editorSv.creaEditorial("ESTRADA");
-        Editorial edit8 =editorSv.creaEditorial("COMBEL");
-        Editorial edit9 =editorSv.creaEditorial("BRUGUERA");
-        Editorial edit10 =editorSv.creaEditorial("COMPANHIA DAS LETRAS");
+        Editorial edit1 = eu.creaEditorial("Minotauro");
+        Editorial edit2 = eu.creaEditorial("Sudamericana");
+        Editorial edit3 = eu.creaEditorial("Penguin Random House");
+        Editorial edit4 = eu.creaEditorial("Bruguera");
+        Editorial edit5 = eu.creaEditorial("Planeta");
+        Editorial edit7 = eu.creaEditorial("Emece");
+        Editorial edit8 = eu.creaEditorial("Siruela");
+        Editorial edit9 = eu.creaEditorial("Companhia das Letras");
+        Editorial edit10 = eu.creaEditorial("New Directions");
+        Editorial edit11 = eu.creaEditorial("Lippincott's Monthly Magazine");
+        Editorial edit12 = eu.creaEditorial("Santillana");
+        Editorial edit13 = eu.creaEditorial("Estrada");
+        Editorial edit14 = eu.creaEditorial("Combel");
         
         
+        Libro lb1 = new Libro("El señor de los anillos", 1937, aut1, edit1);
+        Libro lb2 = new Libro("El señor de los anillos 2", 1939,  aut1, edit1);
+        Libro lb3 = new Libro("El señor de los anillos 3", 1940,  aut1, edit1);
+        Libro lb4 = new Libro("Cien años de soledad", 1967, aut2, edit2);
+        Libro lb5 = new Libro("El amor en tiempos del colera", 1985, aut2, edit3);
+        Libro lb6 = new Libro("Cronica de una muerte anunciada", 1981, aut2, edit4);
+        Libro lb7 = new Libro("Del amor y otros demonios", 1994, aut2, edit2);
+        Libro lb8 = new Libro("El alquimista", 1988,  aut3, edit5);
+        Libro lb9 = new Libro("Veronika decide morir", 1998, aut3, edit5);
+        Libro lb10 = new Libro("Adulterio", 2014, aut3, edit5);
+        Libro lb11 = new Libro("Manual del guerrero de la luz", 1997, aut3, edit7);  
+        Libro lb12 = new Libro("Ficciones", 1944, aut3, edit7);
+        Libro lb13 = new Libro("La biblioteca de Babel", 1941, aut4, edit8);
+        Libro lb14 = new Libro("El libro de arena", 1975, aut4, edit7);
+        Libro lb15 = new Libro("El hacedor", 1917,  aut4, edit2);
+        Libro lb16 = new Libro("Labyrinths", 1962,  aut4, edit10);
+        Libro lb17 = new Libro("El retrato de Dorian Gray", 1890,  aut5, edit11);
+        Libro lb18 = new Libro("El principe feliz y otros cuentos", 1888,  aut5, edit12);
+        Libro lb19 = new Libro("El fantasma de Canterville", 1887, aut5, edit13);
+        Libro lb20 = new Libro("El gigante egoista", 1888, aut5, edit14);
         
         
-        Libro lb1 = lu.creaLibro("El señor de los anillos", 1937, aut1, edit1);
-        Libro lb2 = lu.creaLibro("El señor de los anillos 2", 1939, aut1, edit1);
-        Libro lb3 = lu.creaLibro("El señor de los anillos 3", 1940, aut1, edit1);
-        Libro lb4 = lu.creaLibro("Cien años de soledad", 1967, aut2, edit2);
-        Libro lb5 = lu.creaLibro("El amor en tiempos del colera", 1985, aut2, edit3);
-        Libro lb6 = lu.creaLibro("Cronica de una muerte anunciada", 1981, aut2, edit4);
-        Libro lb7 = lu.creaLibro("Del amor y otros demonios", 1994, aut2, edit2);
-        Libro lb8 = lu.creaLibro("El alquimista", 1988, aut3, edit5);
-        Libro lb9 = lu.creaLibro("Veronika decide morir", 1998, 5, 1, 4, true, aut3, edit5);
-        Libro lb10 = lu.creaLibro("Adulterio", 2014, 5, 4, 1, true, aut3, edit5);
-        Libro lb11 = lu.creaLibro("Manual del guerrero de la luz", 1997, 5, 2, 3, true, aut3, edit7);  
-        Libro lb12 = lu.creaLibro("Ficciones", 1944, 5, 0, 5, true, aut3, edit7);
-        Libro lb13 = lu.creaLibro("La biblioteca de Babel", 1941, 3, 1, 2, true, aut4, edit8);
-        Libro lb14 = lu.creaLibro("El libro de arena", 1975, 7, 3, 4, true, aut4, edit7);
-        Libro lb15 = lu.creaLibro("El hacedor", 1917, 4, 2, 2, true, aut4, edit2);
-        Libro lb16 = lu.creaLibro("Labyrinths", 1962, 6, 2, 4, true, aut4, edit10);
-        Libro lb17 = lu.creaLibro("El retrato de Dorian Gray", 1890, 5, 1, 4, true, aut5, edit11);
-        Libro lb18 = lu.creaLibro("El principe feliz y otros cuentos", 1888, 5, 0, 5, true, aut5, edit12);
-        Libro lb19 = lu.creaLibro("El fantasma de Canterville", 1887, 5, 4, 1, true, aut5, edit13);
-        Libro lb20 = lu.creaLibro("El gigante egoista", 1888, 5, 1, 4, true, aut5, edit14);
-        
-     
+
 
 
     }
