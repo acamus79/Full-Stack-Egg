888e3855e5a31af3c70631c00f74ab27c7e39f0a
acamus79 <adrianedgardocamus@gmail.com>
2021-11-02 20:00:05 -0300

    Create PortalControlador.java

---
+++ Guia 16 Spring/Libreria/src/main/java/com/mza/libreria/controladores/PortalControlador.java
@@ -0,0 +1,14 @@
+package com.mza.libreria.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PortalControlador {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+}
