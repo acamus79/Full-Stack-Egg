ec64656c92b427b3915e3192333bc9464f03e102
acamus79 <adrianedgardocamus@gmail.com>
2021-11-03 08:38:18 -0300

    Update RepoAutor.java

--- Guia 16 Spring/Libreria/src/main/java/com/mza/libreria/repositorios/RepoAutor.java
+++ Guia 16 Spring/Libreria/src/main/java/com/mza/libreria/repositorios/RepoAutor.java
@@ -7,7 +7,10 @@
 package com.mza.libreria.repositorios;
 
 import com.mza.libreria.entidades.Autor;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,5 +19,11 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface RepoAutor extends JpaRepositoryImplementation<Autor, String>{
-
+            @Query ("SELECT a FROM Autor")
+            public List<Autor> buscaAutor();
 }
+
+/*
+   @Query ("SELECT a FROM Autor a WHERE a.nombre LIKE %?1%")
+    public List<Autor> buscarPorAutor(@Param("nombre") String nombre);
+*/
