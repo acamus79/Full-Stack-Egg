a7dcb8711474bf495f07b321feaedc8446f6e826
acamus79 <adrianedgardocamus@gmail.com>
2021-11-06 16:09:37 -0300

    Update Controller

--- Guia 16 Spring/Libreria/src/main/java/com/mza/libreria/controladores/AutorController.java
+++ Guia 16 Spring/Libreria/src/main/java/com/mza/libreria/controladores/AutorController.java
@@ -3,16 +3,32 @@
  * Uso para capacitación
  * 2021 Año de la Prevención y Lucha contra el COVID-19.
  */
-
 package com.mza.libreria.controladores;
 
+import com.mza.libreria.entidades.Autor;
+import com.mza.libreria.servicios.AutorService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * 
+ *
  * @author Adrian E. Camus
  */
 @Controller
 public class AutorController {
+    @Autowired
+    AutorService autorServicio;
+
+    @RequestMapping(value = "/autores", method = RequestMethod.GET)
+    public String launchPage(Model model) {
+        List<Autor> autores = autorServicio.buscaAutor();
+        model.addAttribute("autores", autores);
+
+        return "autores";
+    }
 
 }
