64f13171ebf9eb57ebccdda20601877bd115a600
acamus79 <adrianedgardocamus@gmail.com>
2021-11-06 17:40:04 -0300

    Creacion Entidad Foto Signed-off-by:acamus79 <adrianedgardocamus@gmail.com>

---
+++ Guia 16 Spring/Tinder_de_Mascotas/src/main/java/edu/egg/tinder/entidades/Foto.java
@@ -0,0 +1,93 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package edu.egg.tinder.entidades;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import org.hibernate.annotations.GenericGenerator;
+
+
+
+/**
+ * 
+ * @author Adrian E. Camus
+ */
+@Entity
+public class Foto {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String nombre;
+    private String mime;
+    
+    @Lob @Basic(fetch = FetchType.LAZY)
+    private byte[] contenido;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the mime
+     */
+    public String getMime() {
+        return mime;
+    }
+
+    /**
+     * @param mime the mime to set
+     */
+    public void setMime(String mime) {
+        this.mime = mime;
+    }
+
+    /**
+     * @return the contenido
+     */
+    public byte[] getContenido() {
+        return contenido;
+    }
+
+    /**
+     * @param contenido the contenido to set
+     */
+    public void setContenido(byte[] contenido) {
+        this.contenido = contenido;
+    }
+
+    
+}
