9c2c7e6e30d6a420c9d74c5c1e37c0dde751e5fb
acamus79 <adrianedgardocamus@gmail.com>
2021-11-14 12:59:35 -0300

    Controladores

---
+++ Guia 16 Spring/Tinder_de_Mascotas/src/main/java/edu/egg/tinder/controladores/ControladorMascota.java
@@ -0,0 +1,15 @@
+/*
+ * Sin licencia.
+ * Uso para capacitación
+ * 2021 Año de la Prevención y Lucha contra el COVID-19.
+ */
+
+package edu.egg.tinder.controladores;
+
+/**
+ * 
+ * @author Adrian E. Camus
+ */
+public class ControladorMascota {
+
+}
